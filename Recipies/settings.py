42aeeb569b2dd1619234b65383c8f593ad27935d
vabene1111 <vabene1234@googlemail.com>
2018-02-05 11:26:30 +0100

    secret settings

--- Recipies/settings.py
+++ Recipies/settings.py
@@ -11,15 +11,10 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from Recipies.secret_settings import *
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3*2&bz92brcnq^3c9b=_g%sc3b(i1j#rs*%1k67c8=cy2397j3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,16 +71,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'Recipies.wsgi.application'
-
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
