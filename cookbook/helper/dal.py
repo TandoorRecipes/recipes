804190c2bbbee7269d0eadc6fc4d87d50fbd88e3
vabene1111 <vabene1234@googlemail.com>
2019-11-19 21:16:31 +0100

    dal ingredient endpoint

--- cookbook/helper/dal.py
+++ cookbook/helper/dal.py
@@ -1,6 +1,6 @@
 from dal import autocomplete
 
-from cookbook.models import Keyword
+from cookbook.models import Keyword, RecipeIngredients
 
 
 class KeywordAutocomplete(autocomplete.Select2QuerySetView):
@@ -9,6 +9,19 @@ class KeywordAutocomplete(autocomplete.Select2QuerySetView):
             return Keyword.objects.none()
 
         qs = Keyword.objects.all()
+
+        if self.q:
+            qs = qs.filter(name__istartswith=self.q)
+
+        return qs
+
+
+class IngredientsAutocomplete(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+        if not self.request.user.is_authenticated:
+            return RecipeIngredients.objects.none()
+
+        qs = RecipeIngredients.objects.all()
 
         if self.q:
             qs = qs.filter(name__istartswith=self.q)
