9fa4ee388c8e1b640a012f95002c80933c53b133
vabene1111 <vabene1234@googlemail.com>
2019-12-08 17:28:02 +0100

    Revert "test trigram filter"
    This reverts commit 516b6039f730bdc699d7907d2a1437b8261d1b76.

--- cookbook/filters.py
+++ cookbook/filters.py
@@ -5,7 +5,7 @@ from cookbook.models import Recipe, Keyword
 
 
 class RecipeFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_expr='trigram_similar')
+    name = django_filters.CharFilter(lookup_expr='icontains')
     keywords = django_filters.ModelMultipleChoiceFilter(queryset=Keyword.objects.all(), widget=MultiSelectWidget,
                                                         method='filter_keywords')
 
