3a309bbdf861fa94e41c5cd13fc1754a66b1a26c
vabene1111 <vabene1234@googlemail.com>
2019-12-08 18:33:46 +0100

    add deps

--- cookbook/migrations/0003_enable_pgtrm.py
+++ cookbook/migrations/0003_enable_pgtrm.py
@@ -3,6 +3,10 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
+    dependencies = [
+        ('cookbook', '0002_auto_20191119_2035'),
+    ]
+
     operations = [
         TrigramExtension(),
     ]
