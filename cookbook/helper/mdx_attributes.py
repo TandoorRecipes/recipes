60d5edaf6b4569653a103ccd88668ea2d410a634
vabene1111 <vabene1234@googlemail.com>
2020-02-28 21:53:27 +0100

    improved markdown rendering of tables and images

---
+++ cookbook/helper/mdx_attributes.py
@@ -0,0 +1,24 @@
+import markdown
+
+from markdown.treeprocessors import Treeprocessor
+
+
+class StyleTreeprocessor(Treeprocessor):
+
+    def run_processor(self, node):
+        for child in node:
+            if child.tag == "table":
+                child.set("class", "table table-bordered")
+            if child.tag == "img":
+                child.set("class", "img-fluid")
+            self.run_processor(child)
+        return node
+
+    def run(self, root):
+        self.run_processor(root)
+        return root
+
+
+class MarkdownFormatExtension(markdown.Extension):
+    def extendMarkdown(self, md, md_globals):
+        md.treeprocessors.register(StyleTreeprocessor(), 'StyleTreeprocessor', 10)
