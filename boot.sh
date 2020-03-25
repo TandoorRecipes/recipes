913d858473a1d44b2ced02e09fddfc4d320848b7
vabene1111 <vabene1234@googlemail.com>
2020-03-25 22:30:44 +0100

    updated boot script to fix permission

--- boot.sh
+++ boot.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+
+# Change ownership if needed
+if [ ! -n "$(find staticfiles -user "$(id -u)" -print -prune -o -prune)" ]
+then
+    echo "Changing ownership of staticfiles"
+    chown -R recipes:recipes staticfiles
+fi
+if [ ! -n "$(find mediafiles -user "$(id -u)" -print -prune -o -prune)" ]
+then
+    echo "Changing ownership of mediafiles"
+    chown -R recipes:recipes mediafiles
+fi
+
 source venv/bin/activate
 
 echo "Updating database"
