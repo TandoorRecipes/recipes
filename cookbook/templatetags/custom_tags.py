2c3140248c427909698efac77efab556ca978c02
vabene1111 <vabene1234@googlemail.com>
2020-03-27 21:41:55 +0100

    fixed broken links

--- cookbook/templatetags/custom_tags.py
+++ cookbook/templatetags/custom_tags.py
@@ -29,4 +29,4 @@ def delete_url(model, pk):
 def markdown(value):
     tags = markdown_tags + ['pre', 'table', 'td', 'tr', 'th', 'tbody', 'style', 'thead']
     parsed_md = md.markdown(value, extensions=['markdown.extensions.fenced_code', 'tables', MarkdownFormatExtension()])
-    return bleach.clean(parsed_md, tags, print_attrs, markdown_attrs)
+    return bleach.clean(parsed_md, tags, markdown_attrs)
