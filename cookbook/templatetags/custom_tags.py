f00ee7d9fada05cdc2b549c7af8da95d3f8272ee
vabene1111 <vabene1234@googlemail.com>
2020-05-02 21:44:12 +0200

    display log info

--- cookbook/templatetags/custom_tags.py
+++ cookbook/templatetags/custom_tags.py
@@ -1,12 +1,13 @@
-from django import template
-import markdown as md
 import bleach
-from bleach_whitelist import markdown_tags, markdown_attrs, all_styles, print_attrs
+import markdown as md
+from bleach_whitelist import markdown_tags, markdown_attrs
+from django import template
+from django.db.models import Avg
 from django.urls import reverse
 
 from cookbook.helper.mdx_attributes import MarkdownFormatExtension
 from cookbook.helper.mdx_urlize import UrlizeExtension
-from cookbook.models import get_model_name
+from cookbook.models import get_model_name, CookLog
 
 register = template.Library()
 
@@ -29,6 +30,32 @@ def delete_url(model, pk):
 @register.filter()
 def markdown(value):
     tags = markdown_tags + ['pre', 'table', 'td', 'tr', 'th', 'tbody', 'style', 'thead']
-    parsed_md = md.markdown(value, extensions=['markdown.extensions.fenced_code', 'tables',  UrlizeExtension(), MarkdownFormatExtension()])
+    parsed_md = md.markdown(value, extensions=['markdown.extensions.fenced_code', 'tables', UrlizeExtension(), MarkdownFormatExtension()])
     markdown_attrs['*'] = markdown_attrs['*'] + ['class']
     return bleach.clean(parsed_md, tags, markdown_attrs)
+
+
+@register.simple_tag
+def recipe_rating(recipe, user):
+    rating = recipe.cooklog_set.filter(created_by=user).aggregate(Avg('rating'))
+    if rating['rating__avg']:
+
+        rating_stars = ''
+        for i in range(int(rating['rating__avg'])):
+            rating_stars = rating_stars + '<i class="fas fa-star fa-xs"></i>'
+
+        if rating['rating__avg'] % 1 >= 0.5:
+            rating_stars = rating_stars + '<i class="fas fa-star-half-alt fa-xs"></i>'
+
+        return rating_stars
+    else:
+        return ''
+
+
+@register.simple_tag
+def recipe_last(recipe, user):
+    last = recipe.cooklog_set.filter(created_by=user).last()
+    if last:
+        return last.created_at
+    else:
+        return ''
