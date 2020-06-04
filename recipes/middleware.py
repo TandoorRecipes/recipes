c85adef6201a67154dc172b7a9012819f8e7f381
Brendan Cazier <520246+cazier@users.noreply.github.com>
2020-06-04 07:51:43 -0500

    Re-adding import statement

--- recipes/middleware.py
+++ recipes/middleware.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.middleware import RemoteUserMiddleware
+from os import getenv
 
 class CustomRemoteUser(RemoteUserMiddleware):
     header = getenv('PROXY_HEADER', 'HTTP_REMOTE_USER')
