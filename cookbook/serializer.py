2bda5bbbf7f6e0ae283dd845c15ce1d6574965df
vabene1111 <vabene1234@googlemail.com>
2020-06-09 20:27:31 +0200

    note text saving

--- cookbook/serializer.py
+++ cookbook/serializer.py
@@ -9,7 +9,7 @@ class MealPlanSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = MealPlan
-        fields = ('id', 'title', 'recipe', 'date', 'meal_type', 'created_by', 'recipe_name', 'meal_type_name')
+        fields = ('id', 'title', 'recipe', 'note', 'date', 'meal_type', 'created_by', 'recipe_name', 'meal_type_name')
 
 
 class MealTypeSerializer(serializers.ModelSerializer):
