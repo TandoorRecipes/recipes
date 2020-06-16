17946c8dac33468b7adf14d699736ad9ff2afa98
vabene1111 <vabene1234@googlemail.com>
2020-06-16 10:37:11 +0200

    markdown hint text

--- cookbook/forms.py
+++ cookbook/forms.py
@@ -268,6 +268,7 @@ class MealPlanForm(forms.ModelForm):
 
         help_texts = {
             'shared': _('You can list default users to share recipes with in the settings.'),
+            'note': _('You can use markdown to format this field. See the <a href="/docs/markdown/">docs here</a>')
         }
 
         widgets = {'recipe': SelectWidget, 'date': DateWidget, 'shared': MultiSelectWidget}
