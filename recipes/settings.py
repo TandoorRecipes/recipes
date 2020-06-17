e433976d2883e7743057196c4b5ba72a1705b255
vabene1111 <vabene1234@googlemail.com>
2020-06-17 10:12:27 +0200

    custom browsable api header

--- recipes/settings.py
+++ recipes/settings.py
@@ -102,7 +102,7 @@ ROOT_URLCONF = 'recipes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'cookbook', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
