60d7c75d214de71e91da0f37c10e5a96e7f791b5
vabene1111 <vabene1234@googlemail.com>
2020-06-29 18:11:25 +0200

    basic shopping list fix

--- cookbook/models.py
+++ cookbook/models.py
@@ -189,6 +189,9 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Comment(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
