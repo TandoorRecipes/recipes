9b9fbd5cabe94bdadeb65c93238b37509bb5ce47
vabene1111 <vabene1234@googlemail.com>
2020-07-09 22:19:24 +0200

    added food api view tests

--- cookbook/tests/api/test_api_food.py
+++ cookbook/tests/api/test_api_food.py
@@ -2,7 +2,7 @@ import json
 
 from django.urls import reverse
 
-from cookbook.models import Unit
+from cookbook.models import Food
 from cookbook.tests.views.test_views import TestViews
 
 
@@ -10,44 +10,44 @@ class TestApiUnit(TestViews):
 
     def setUp(self):
         super(TestApiUnit, self).setUp()
-        self.unit_1 = Unit.objects.create(
-            name='kg'
+        self.food_1 = Food.objects.create(
+            name='Beef'
         )
-        self.unit_2 = Unit.objects.create(
-            name='g'
+        self.food_2 = Food.objects.create(
+            name='Chicken'
         )
 
     def test_keyword_list(self):
         # verify view permissions are applied accordingly
         self.batch_requests([(self.anonymous_client, 403), (self.guest_client_1, 403), (self.user_client_1, 200), (self.admin_client_1, 200), (self.superuser_client, 200)],
-                            reverse('api:unit-list'))
+                            reverse('api:food-list'))
 
         # verify storage is returned
-        r = self.user_client_1.get(reverse('api:unit-list'))
+        r = self.user_client_1.get(reverse('api:food-list'))
         self.assertEqual(r.status_code, 200)
         response = json.loads(r.content)
         self.assertEqual(len(response), 2)
-        self.assertEqual(response[0]['name'], self.unit_1.name)
+        self.assertEqual(response[0]['name'], self.food_1.name)
 
-        r = self.user_client_1.get(f'{reverse("api:unit-list")}?limit=1')
+        r = self.user_client_1.get(f'{reverse("api:food-list")}?limit=1')
         response = json.loads(r.content)
         self.assertEqual(len(response), 1)
 
-        r = self.user_client_1.get(f'{reverse("api:unit-list")}?query=m')
+        r = self.user_client_1.get(f'{reverse("api:food-list")}?query=Pork')
         response = json.loads(r.content)
         self.assertEqual(len(response), 0)
 
-        r = self.user_client_1.get(f'{reverse("api:unit-list")}?query=kg')
+        r = self.user_client_1.get(f'{reverse("api:food-list")}?query=Beef')
         response = json.loads(r.content)
         self.assertEqual(len(response), 1)
 
     def test_keyword_update(self):
-        r = self.user_client_1.patch(reverse('api:unit-detail', args={self.unit_1.id}), {'name': 'new'}, content_type='application/json')
+        r = self.user_client_1.patch(reverse('api:food-detail', args={self.food_1.id}), {'name': 'new'}, content_type='application/json')
         response = json.loads(r.content)
         self.assertEqual(r.status_code, 200)
         self.assertEqual(response['name'], 'new')
 
     def test_keyword_delete(self):
-        r = self.user_client_1.delete(reverse('api:unit-detail', args={self.unit_1.id}))
+        r = self.user_client_1.delete(reverse('api:food-detail', args={self.food_1.id}))
         self.assertEqual(r.status_code, 204)
-        self.assertEqual(Unit.objects.count(), 1)
+        self.assertEqual(Food.objects.count(), 1)
