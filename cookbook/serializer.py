d6582ea79d8f248c6373d5de95e253c431e95b23
vabene1111 <vabene1234@googlemail.com>
2020-07-09 22:00:14 +0200

    added keyword api tests

--- cookbook/serializer.py
+++ cookbook/serializer.py
@@ -81,7 +81,7 @@ class KeywordSerializer(UniqueFieldsMixin, serializers.ModelSerializer):
 
     class Meta:
         model = Keyword
-        fields = ('id', 'name', 'icon', 'label', 'description', 'created_by', 'created_at', 'updated_at')
+        fields = ('id', 'name', 'icon', 'label', 'description', 'created_at', 'updated_at')
 
         read_only_fields = ('id',)
 
