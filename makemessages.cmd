0235fb47917251fe8ebe2b80c8c827031b047cad
vabene1111 <vabene1234@googlemail.com>
2020-12-26 13:48:33 +0100

    updated base translations

--- makemessages.cmd
+++ makemessages.cmd
@@ -1,2 +1,2 @@
 CALL venv\Scripts\activate.bat
-python manage.py makemessages -i venv -l de -l nl -l rn -l fr -l tr -l pt -l en
+python manage.py makemessages -i venv -l de -l nl -l rn -l fr -l tr -l pt -l en -l it -l ca -l es -l zh_CN -l hu_HU
