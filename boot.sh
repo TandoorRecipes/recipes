ea30eb96cdd27ba9b710a729b0714d370f8d5cf5
vabene1111 <vabene1234@googlemail.com>
2021-01-04 23:40:58 +0100

    changed up structure again

--- boot.sh
+++ boot.sh
@@ -8,7 +8,4 @@ echo "Done"
 
 chmod -R 755 /opt/recipes/mediafiles
 
-mkdir -p /opt/recipes/nginx
-cp -u -p /opt/recipes/Recipes.conf /opt/recipes/nginx
-
 exec gunicorn -b :8080 --access-logfile - --error-logfile - --log-level INFO recipes.wsgi
