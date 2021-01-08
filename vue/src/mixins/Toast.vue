976cedd536ed5f0e76b3f8ccc83df1e5cdedc6e4
vabene1111 <vabene1234@googlemail.com>
2021-01-08 22:31:48 +0100

    temp save vue transition stuff

---
+++ vue/src/mixins/Toast.vue
@@ -0,0 +1,14 @@
+<script>
+let toast_mixin = Vue.mixin({
+  methods: {
+    makeToast: function (title, message, variant = null) {
+      this.$bvToast.toast(message, {
+        title: title,
+        variant: variant,
+        toaster: 'b-toaster-top-center',
+        solid: true
+      })
+    }
+  }
+})
+</script>
