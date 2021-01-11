734d4bbbea253ac70cfb111d59750ca2d567cbd0
vabene1111 <vabene1234@googlemail.com>
2021-01-11 18:31:06 +0100

    working on a recipe view

---
+++ vue/src/components/Step.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+
+    <div v-for="i in step.ingredients" v-bind:key="i.id">
+      <Ingredient v-bind:ingredient="i" v-bind:servings="servings"></Ingredient>
+    </div>
+
+    <br/>
+
+    Servings Step: {{servings}}
+
+    {{ step.instruction }}
+
+    <br/>
+    <br/>
+  </div>
+
+</template>
+
+<script>
+
+import Ingredient from "@/components/Ingredient";
+
+export default {
+  name: 'Step',
+  components: {
+    Ingredient
+  },
+  props: {
+    step: Object,
+    servings: Number,
+  }
+}
+</script>
