cb913f6ceadb9d375b91c4e2448d1a53cadfd888
vabene1111 <vabene1234@googlemail.com>
2021-01-11 15:04:35 +0100

    some test code and playing around

---
+++ vue/src/utils.js
@@ -0,0 +1,9 @@
+export {myOtherTestFunction} from './test.js'
+
+import TestComponent from './components/TestComponent'
+
+
+export function myCustomTestFunction(x) {
+    console.log(x)
+    return x
+}
