514a048cfc383a9e5606b7f75c282127dfff99dc
vabene1111 <vabene1234@googlemail.com>
2021-01-13 20:59:31 +0100

    added loading spinner and made new view the main recipe view

---
+++ vue/src/components/LoadingSpinner.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="row">
+      <div class="col" style="text-align: center">
+        <i class="fas fa-spinner fa-spin fa-10x"></i>
+      </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'LoadingSpinner',
+  props: {
+    recipe: Object,
+  },
+}
+</script>
