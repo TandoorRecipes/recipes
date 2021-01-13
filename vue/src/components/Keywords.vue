948eb9be3e4208a61c66820ed64b3517e9a52e48
vabene1111 <vabene1234@googlemail.com>
2021-01-13 20:49:04 +0100

    keywords

---
+++ vue/src/components/Keywords.vue
@@ -0,0 +1,17 @@
+<template>
+    <div v-if="recipe.keywords.length > 0">
+      <small :key="k.id" v-for="k in recipe.keywords" style="padding: 2px">
+        {{k.icon}} {{k.name}}
+      </small>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Keywords',
+  props: {
+    recipe: Object,
+  },
+}
+</script>
