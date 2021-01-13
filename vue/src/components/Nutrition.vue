449fa210e13159eb901ca4226508562ed95eb77c
vabene1111 <vabene1234@googlemail.com>
2021-01-13 10:57:38 +0100

    recipe page styling

--- vue/src/components/Nutrition.vue
+++ vue/src/components/Nutrition.vue
@@ -6,67 +6,50 @@
       <div class="card-body">
         <div class="row">
           <div class="col-12">
-            <h5><i class="fas fa-carrot"></i> {{ _('Nutrition') }}</h5>
+            <h4 class="card-title"><i class="fas fa-carrot"></i> {{ _('Nutrition') }}</h4>
           </div>
         </div>
 
-        <div class="d-flex flex-row">
-
-        </div>
-
-
         <div class="row">
-          <div class="col-12">
-            {{ _('Calories') }}
+          <div class="col-6">
+            <i class="fas fa-fire fa-fw"></i> {{ _('Calories') }}
+          </div>
+          <div class="col-6">
             {{ calculateAmount(recipe.nutrition.calories) }} kcal
           </div>
         </div>
 
         <div class="row">
-          <div class="col-12">
-            {{ _('Carbohydrates') }}
+          <div class="col-6">
+            <i class="fas fa-bread-slice fa-fw"></i> {{ _('Carbohydrates') }}
+          </div>
+          <div class="col-6">
+
             {{ calculateAmount(recipe.nutrition.carbohydrates) }} g
           </div>
         </div>
 
-        <div class="d-flex flex-row">
-          <div class="flex-column">
-            <div class="">
-              {{ _('Fats') }}
-            </div>
-            <div class="">
-              {{ calculateAmount(recipe.nutrition.fats) }}
-            </div>
-            <div class="">
-              g
-            </div>
+        <div class="row">
+          <div class="col-6">
+            <i class="fas fa-cheese fa-fw"></i> {{ _('Fats') }}
           </div>
-
-          <div class="flex-column">
-            <div class="">
-              {{ _('Fats') }}
-            </div>
-            <div class="">
-              {{ calculateAmount(recipe.nutrition.fats) }}
-            </div>
-            <div class="">
-              g
-            </div>
+          <div class="col-6">
+            {{ calculateAmount(recipe.nutrition.fats) }} g
           </div>
-
         </div>
 
         <div class="row">
-          <div class="col-12">
-            {{ _('Proteins') }}
+          <div class="col-6">
+            <i class="fas fa-drumstick-bite fa-fw"></i> {{ _('Proteins') }}
+          </div>
+          <div class="col-6">
             {{ calculateAmount(recipe.nutrition.proteins) }} g
           </div>
         </div>
-
+      </div>
 
       </div>
     </div>
-  </div>
 
 </template>
 
