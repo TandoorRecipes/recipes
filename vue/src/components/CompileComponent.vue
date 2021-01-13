cd51d126182f895aed28b77282533fb2566fbcf7
vabene1111 <vabene1234@googlemail.com>
2021-01-13 01:16:52 +0100

    still working

--- vue/src/components/CompileComponent.vue
+++ vue/src/components/CompileComponent.vue
@@ -1,5 +1,4 @@
 <template>
-
   <div>
     <component :is="compiled" :servings="servings" :code="code"></component>
   </div>
@@ -10,35 +9,23 @@ import Vue from 'vue'
 
 import ScalableNumber from "@/components/ScalableNumber";
 
-var res = Vue.compile('<div><span>{{servings}}  Test <scalable-number v-bind:number="2" :factor="servings"></scalable-number></span></div>')
-
 export default {
   name: 'CompileComponent',
   props: ['code', 'servings'],
   data() {
     return {
       compiled: null,
-      test: '<div><p>Alle Zutaten vermischen und an einen <strong>warmen Ort</strong> stelllen <scalable-number v-bind:number="10.0000000000000000" v-bind:factor="servings"></scalable-number> g Anstellgut</p></div>'
     }
   },
-  components: {
-    ScalableNumber, // eslint-disable-line
-  },
   mounted() {
-    setTimeout(() => {
-      this.compiled = Vue.component('button-counter', {
-        props: ['servings', 'code'],
-        components: {
-          ScalableNumber, // eslint-disable-line
-        },
-        data: function () {
-          return {
-            count: 0
-          }
-        },
-        template: `<div>${this.code}</div>`
-      })
-    }, 500)
+    this.compiled = Vue.component('compiled-component', {
+      props: ['servings', 'code'],
+      components: {
+        ScalableNumber, // eslint-disable-line
+      },
+      template: `
+        <div>${this.code}</div>`
+    })
 
   }
 }
