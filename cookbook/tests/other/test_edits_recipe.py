5bc282987fb596a1b950f9c1a8fafaa3560a8305
vabene1111 <vabene1234@googlemail.com>
2021-01-19 21:26:20 +0100

    ingredient linking fixed + tests fixed

--- cookbook/tests/other/test_edits_recipe.py
+++ cookbook/tests/other/test_edits_recipe.py
@@ -10,14 +10,14 @@ class TestEditsRecipe(TestBase):
     # flake8: noqa
     def test_ld_json(self):
         test_list = [
-            {'file': 'cookbook/tests/resources/websites/ld_json_1.html', 'result_length': 3218},
+            {'file': 'cookbook/tests/resources/websites/ld_json_1.html', 'result_length': 3222},
             {'file': 'cookbook/tests/resources/websites/ld_json_2.html', 'result_length': 1510},
             {'file': 'cookbook/tests/resources/websites/ld_json_3.html', 'result_length': 1629},
             {'file': 'cookbook/tests/resources/websites/ld_json_4.html', 'result_length': 1729},
-            {'file': 'cookbook/tests/resources/websites/ld_json_itemList.html', 'result_length': 3200},
+            {'file': 'cookbook/tests/resources/websites/ld_json_itemList.html', 'result_length': 3206},
             {'file': 'cookbook/tests/resources/websites/ld_json_multiple.html', 'result_length': 1606},
             {'file': 'cookbook/tests/resources/websites/micro_data_1.html', 'result_length': 1079},
-            {'file': 'cookbook/tests/resources/websites/micro_data_2.html', 'result_length': 1429},
+            {'file': 'cookbook/tests/resources/websites/micro_data_2.html', 'result_length': 1438},
             {'file': 'cookbook/tests/resources/websites/micro_data_3.html', 'result_length': 1148},
             {'file': 'cookbook/tests/resources/websites/micro_data_4.html', 'result_length': 4396},
         ]
