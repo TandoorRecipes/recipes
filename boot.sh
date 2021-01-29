d2b095910d3a01f7270dd97e27fdda7638e3fa69
Christoph Schöni <christoph.schoeni@gmail.com>
2021-01-29 14:19:20 +0100

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,20 +1,4 @@
 #!/bin/sh
-
-set -e
-  
-host="$1"
-shift
-cmd="$@"
-  
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "postgres" -c '\q'; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
-  
->&2 echo "Postgres is up - executing command"
-exec $cmd
-
-
 source venv/bin/activate
 
 echo "Updating database"
