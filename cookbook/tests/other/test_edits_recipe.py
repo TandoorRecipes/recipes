59b2da933da4be8a5084134e3b679777567c8c29
vabene1111 <vabene1234@googlemail.com>
2021-02-25 08:27:22 +0100

    fixed tests

--- cookbook/tests/other/test_edits_recipe.py
+++ cookbook/tests/other/test_edits_recipe.py
@@ -14,7 +14,7 @@ class TestEditsRecipe(TestBase):
             {'file': 'cookbook/tests/resources/websites/ld_json_2.html', 'result_length': 1525},
             {'file': 'cookbook/tests/resources/websites/ld_json_3.html', 'result_length': 1644},
             {'file': 'cookbook/tests/resources/websites/ld_json_4.html', 'result_length': 1744},
-            {'file': 'cookbook/tests/resources/websites/ld_json_itemList.html', 'result_length': 3221},
+            {'file': 'cookbook/tests/resources/websites/ld_json_itemList.html', 'result_length': 3222},
             {'file': 'cookbook/tests/resources/websites/ld_json_multiple.html', 'result_length': 1621},
             {'file': 'cookbook/tests/resources/websites/micro_data_1.html', 'result_length': 1094},
             {'file': 'cookbook/tests/resources/websites/micro_data_2.html', 'result_length': 1453},
