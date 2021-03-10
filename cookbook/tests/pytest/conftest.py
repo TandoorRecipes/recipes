a8cbbafb1c73582ced48f829ee9c7a56d83c8416
vabene1111 <vabene1234@googlemail.com>
2021-03-10 18:18:19 +0100

    tests basically working

--- cookbook/tests/pytest/conftest.py
+++ cookbook/tests/pytest/conftest.py
@@ -1,3 +1,4 @@
+import copy
 import inspect
 import uuid
 
@@ -6,6 +7,11 @@ from django.contrib.auth.models import User, Group
 from django_scopes import scopes_disabled
 
 from cookbook.models import Space
+
+
+@pytest.fixture(autouse=True)
+def enable_db_access_for_all_tests(db):
+    pass
 
 
 @pytest.fixture
@@ -25,34 +31,38 @@ def create_user(db, django_user_model, test_password):
 
 
 @pytest.fixture
-def space_1(client):
+def space_1():
     with scopes_disabled():
         return Space.objects.get_or_create(name='space_1')[0]
 
 
 @pytest.fixture
-def space_2(client):
+def space_2():
     with scopes_disabled():
         return Space.objects.get_or_create(name='space_2')[0]
 
 
 @pytest.fixture
 def user_1(client, space_1):
+    c = copy.deepcopy(client)
     with scopes_disabled():
+        print(f'creating user 1 with space {space_1}')
         user = User.objects.create(username='user_1')
         user.groups.add(Group.objects.get(name='user'))
         user.userpreference.space = space_1
         user.userpreference.save()
-        client.force_login(user)
-        return client
+        c.force_login(user)
+        return c
 
 
 @pytest.fixture
 def user_2(client, space_2):
+    c = copy.deepcopy(client)
     with scopes_disabled():
+        print(f'creating user 2 with space {space_2}')
         user = User.objects.create(username='user_2')
         user.groups.add(Group.objects.get(name='user'))
         user.userpreference.space = space_2
         user.userpreference.save()
-        client.force_login(user)
-        return client
+        c.force_login(user)
+        return c
--- cookbook/tests/pytest/test_test.py
+++ cookbook/tests/pytest/test_test.py
@@ -10,11 +10,16 @@ from cookbook.models import Keyword, Space
 
 
 @pytest.mark.django_db
-def test_user_create(user_1, user_2):
+def test_user_create(user_2, user_1):
     r = user_1.post(reverse('api:keyword-list'), {'name': 'test', 'space': 1}, content_type='application/json')
     response = json.loads(r.content)
     assert r.status_code == 201
     assert response['name'] == 'test'
+
+    print('DEEEEEBUUUUUGGGG')
+    print(user_1.get(reverse('view_test')).content)
+    print(user_2.get(reverse('view_test')).content)
+    print(user_2)
 
     r = user_2.get(reverse('api:keyword-detail', args={response['id']}), content_type='application/json')
     assert r.status_code == 404
