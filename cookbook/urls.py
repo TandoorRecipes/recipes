a37b29d9dc6bb4004518d8e28b409659d2339e8d
smilerz <smilerz@gmail.com>
2021-03-25 17:48:32 -0500

    bookmarklet passing data to form

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -96,6 +96,7 @@ urlpatterns = [
     path('api/recipe-from-source/', api.recipe_from_source, name='api_recipe_from_source'),
     path('api/backup/', api.get_backup, name='api_backup'),
     path('api/ingredient-from-string/', api.ingredient_from_string, name='api_ingredient_from_string'),
+    path('api/bookmarklet/', api.bookmarklet, name='api_bookmarklet'),
 
     path('dal/keyword/', dal.KeywordAutocomplete.as_view(), name='dal_keyword'),
     path('dal/food/', dal.IngredientsAutocomplete.as_view(), name='dal_food'),
