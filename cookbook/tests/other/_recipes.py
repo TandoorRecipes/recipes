f99790975336a3085f3957ec6fc1d612d1d4c4b0
smilerz <smilerz@gmail.com>
2021-04-05 09:20:15 -0500

    add data for url_import test

---
+++ cookbook/tests/other/_recipes.py
@@ -0,0 +1,64 @@
+TEST_LIST = [
+    'madamedessert.html','https://madamedessert.de/schokoladenpudding-rezept-mit-echter-schokolade/#webpage',
+    'madamedessert.json','https://madamedessert.de/schokoladenpudding-rezept-mit-echter-schokolade/#webpage'
+    'cookscountry.html','http://www.cookscountry.com/recipes/2202-shrimp-piccata-pasta'
+    'americastestkitchen.html','http://www.americastestkitchen.com/recipes/1771-pasta-with-chicken-broccoli-and-sun-dried-tomatoes',
+    'cookpad.html','https://cookpad.com/us/recipes/14815875-chicken-and-moringa-drumsticks-soup',
+    'allrecipes.html','https://www.allrecipes.com/recipe/24010/easy-chicken-marsala/',
+    'chefkoch.html','https://www.chefkoch.de/magazin/artikel/1566,0/Chefkoch/Couscous-so-pfiffig-schmeckt-der-Sommer.html',
+    'foodnetwork.com','https://www.foodnetwork.com/recipes/bobby-flay/cast-iron-home-fries-recipe-1945083',
+    'giallozafferano.html','https://ricette.giallozafferano.it/Strangolapreti-alla-trentina.html',
+    'journaldesfemmes.html','https://cuisine.journaldesfemmes.fr/recette/317747-ratatouille',
+    'marmiton.html','https://www.marmiton.org/recettes/recette_fricassee-d-agneau-a-l-oseille_22719.aspx',
+    'delish.html','https://www.delish.com/cooking/recipe-ideas/recipes/a52405/cheesy-baked-asparagus-recipe/',
+    'tudogostoso.html','https://www.tudogostoso.com.br/receita/146568-arroz-com-bacalhau-tomate-e-ervas.html',
+    'tasteofhome.html','https://www.tasteofhome.com/collection/rhubarb-recipes-for-spring/'
+]
+
+ALLRECIPES = {
+
+}
+
+AMERICAS_TEST_KITCHEN = {
+
+}
+
+CHEF_KOCH = {
+
+}
+
+COOKPAD = {
+
+}
+
+DELISH = {
+
+}
+
+FOOD_NETWORK = {
+
+}
+
+GIALLOZAFFERANO = {
+
+}
+
+JOURNAL_DES_FEMMES = {
+
+}
+
+MADAME_DESSERT = {
+
+}
+
+MARMITON = {
+
+}
+
+TASTE_OF_HOME = {
+
+}
+
+TEDOGOSTOSO = {
+
+}
