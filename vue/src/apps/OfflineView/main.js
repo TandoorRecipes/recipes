cce373a52211082b214c631c92be29ce3729eea7
vabene1111 <vabene1234@googlemail.com>
2021-04-11 17:02:52 +0200

    started migrating frontend localization

--- vue/src/apps/OfflineView/main.js
+++ vue/src/apps/OfflineView/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './OfflineView.vue'
+import i18n from "@/i18n";
 
 Vue.config.productionTip = false
 
 new Vue({
+    i18n,
     render: h => h(App),
 }).$mount('#app')
--- vue/vue.config.js
+++ vue/vue.config.js
@@ -44,6 +44,14 @@ module.exports = {
             ],
         }
     },
+    pluginOptions: {
+        i18n: {
+            locale: 'en',
+            fallbackLocale: 'en',
+            localeDir: 'locales',
+            enableInSFC: true
+        }
+    },
     chainWebpack: config => {
 
         config.optimization.splitChunks({
