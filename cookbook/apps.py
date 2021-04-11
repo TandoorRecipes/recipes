b6868c4e5928f75a326cead23d2b5c754576bd0c
smilerz <smilerz@gmail.com>
2021-04-11 17:01:18 -0500

    updated to handle sqlite

--- cookbook/apps.py
+++ cookbook/apps.py
@@ -1,8 +1,11 @@
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class CookbookConfig(AppConfig):
     name = 'cookbook'
 
     def ready(self):
-        import cookbook.signals  # noqa
+        # post_save signal is only necessary if using full-text search on postgres
+        if settings.DATABASES['default']['ENGINE'] in ['django.db.backends.postgresql_psycopg2', 'django.db.backends.postgresql']:
+            import cookbook.signals  # noqa
