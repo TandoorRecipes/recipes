a376c3a5b651808c9a34a8cb9325a32d80e9c9f6
vabene1111 <vabene1234@googlemail.com>
2021-04-18 17:55:48 +0200

    meal plan random fix

--- cookbook/helper/recipe_search.py
+++ cookbook/helper/recipe_search.py
@@ -17,6 +17,7 @@ def search_recipes(queryset, params):
     search_books_or = params.get('books_or', True)
 
     search_internal = params.get('internal', None)
+    search_random = params.get('random', False)
 
     if settings.DATABASES['default']['ENGINE'] in ['django.db.backends.postgresql_psycopg2', 'django.db.backends.postgresql']:
         queryset = queryset.annotate(similarity=TrigramSimilarity('name', search_string), ).filter(Q(similarity__gt=0.1) | Q(name__unaccent__icontains=search_string)).order_by('-similarity')
@@ -48,5 +49,8 @@ def search_recipes(queryset, params):
 
     if search_internal == 'true':
         queryset = queryset.filter(internal=True)
+
+    if search_random == 'true':
+        queryset = queryset.order_by("?")
 
     return queryset
