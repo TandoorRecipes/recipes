6ff9942f837d165790b141bd3b037d1418300c6d
Kaibu <notkaibu@gmail.com>
2021-06-05 18:10:10 +0200

    style fixes

--- vue/src/components/LoadingSpinner.vue
+++ vue/src/components/LoadingSpinner.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row">
       <div class="col" style="text-align: center">
-        <i class="fas fa-spinner fa-spin fa-10x"></i>
+          <img class="spinner-tandoor" alt="loading spinner" src="" style="height: 30vh"/>
       </div>
     </div>
 </template>
