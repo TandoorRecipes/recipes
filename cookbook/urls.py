cdf447634560116f3c454b34454e87fb3cabde58
vabene1111 <vabene1234@googlemail.com>
2021-06-15 20:57:25 +0200

    recipe share link in modal

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -106,6 +106,7 @@ urlpatterns = [
     path('api/recipe-from-source/', api.recipe_from_source, name='api_recipe_from_source'),
     path('api/backup/', api.get_backup, name='api_backup'),
     path('api/ingredient-from-string/', api.ingredient_from_string, name='api_ingredient_from_string'),
+    path('api/share-link/<int:pk>', api.share_link, name='api_share_link'),
 
     path('dal/keyword/', dal.KeywordAutocomplete.as_view(), name='dal_keyword'),
     path('dal/food/', dal.IngredientsAutocomplete.as_view(), name='dal_food'),
