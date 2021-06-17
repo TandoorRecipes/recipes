067eeaaadb6dc6dafe76dce79afc3a291a5f9bdf
vabene1111 <vabene1234@googlemail.com>
2021-06-17 16:05:18 +0200

    django needs two files

--- cookbook/migrations/0137_auto_20210617_1501.py
+++ cookbook/migrations/0137_auto_20210617_1501.py
@@ -30,19 +30,5 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunPython(migrate_spaces),
-        migrations.AlterField(
-            model_name='ingredient',
-            name='space',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='cookbook.space'),
-        ),
-        migrations.AlterField(
-            model_name='nutritioninformation',
-            name='space',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='cookbook.space'),
-        ),
-        migrations.AlterField(
-            model_name='step',
-            name='space',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='cookbook.space'),
-        ),
+
     ]
