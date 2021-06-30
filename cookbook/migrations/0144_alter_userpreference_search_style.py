0394f858a41dc72d711fd6cbfd637bbb9ad4ba77
vabene1111 <vabene1234@googlemail.com>
2021-06-30 15:33:35 +0200

    fixed migration order and dependencies

--- cookbook/migrations/0144_alter_userpreference_search_style.py
+++ cookbook/migrations/0144_alter_userpreference_search_style.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('cookbook', '0123_invitelink_email'),
+        ('cookbook', '0143_create_searchfields'),
     ]
 
     operations = [
