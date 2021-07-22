359e6c64d9c750467dbd0f3f869b65c62ab28943
vabene1111 <vabene1234@googlemail.com>
2021-07-22 16:53:20 +0200

    test create pinned recipe component

---
+++ vue/src/components/PinnedRecipeBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-header"
+         style="position: sticky; top: 0; left: 0; z-index: 1000;">
+
+
+      <div class="collapse navbar-collapse" id="navbarText">
+      </div>
+    </nav>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PinnedRecipeBar"
+}
+</script>
+
+<style scoped>
+
+</style>
