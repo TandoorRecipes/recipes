82ffbf637bb623b97d8db8a74ae4c7c6e617a423
smilerz <smilerz@gmail.com>
2021-08-26 22:04:59 -0500

    Squashed commit of the following:
    commit 7e9a8d4d8690cba68bd6f475b79b206af1598fea
    Author: smilerz <smilerz@gmail.com>
    Date:   Thu Aug 26 21:56:59 2021 -0500
    
        test case to detect 833
    
    commit 75e46b80230b5337ab2e79fd75a07cf6b7610aec
    Author: smilerz <smilerz@gmail.com>
    Date:   Wed Aug 25 21:27:31 2021 -0500
    
        fix #883
    
    commit 2a1e1953ee6ddf5c9c2a57240ff9e1928a4a5122
    Author: smilerz <smilerz@gmail.com>
    Date:   Wed Aug 25 21:08:35 2021 -0500
    
        consolidated recently viewed into FTS queryset
    
    commit aa2d0eafb121069adc1b17a79a07a06afcfed71c
    Author: smilerz <smilerz@gmail.com>
    Date:   Wed Aug 25 16:43:52 2021 -0500
    
        fixed cookie reactivity
    
    commit b74fdb382556feedd8287915ea86611b3adbf181
    Author: smilerz <smilerz@gmail.com>
    Date:   Wed Aug 25 16:37:43 2021 -0500
    
        exact match appears first on keyword/food/unit searches
    
    commit b00c189fd6815537c5232ac0ab5254ca77943b30
    Author: smilerz <smilerz@gmail.com>
    Date:   Wed Aug 25 16:36:14 2021 -0500
    
        add pagination reset on search query change

--- vue/src/apps/RecipeSearchView/RecipeSearchView.vue
+++ vue/src/apps/RecipeSearchView/RecipeSearchView.vue
@@ -226,14 +226,11 @@
                 <recipe-card v-bind:key="`mp_${m.id}`" v-for="m in meal_plans" :recipe="m.recipe"
                              :meal_plan="m" :footer_text="m.meal_type_name"
                              footer_icon="far fa-calendar-alt"></recipe-card>
-
-                <recipe-card v-for="r in recipes" v-bind:key="`rv_${r.id}`" :recipe="r"
+              </template>
+              <recipe-card v-for="r in recipes" v-bind:key="r.id" :recipe="r"
                              :footer_text="isRecentOrNew(r)[0]" 
                              :footer_icon="isRecentOrNew(r)[1]">
-                </recipe-card>
-              </template>
-
-              <!-- <recipe-card v-for="r in recipes" v-bind:key="r.id" :recipe="r"></recipe-card> -->
+              </recipe-card>
             </div>
           </div>
         </div>
