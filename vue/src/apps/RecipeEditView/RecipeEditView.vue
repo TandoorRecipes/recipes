4fbf080e2f6abc19a6be72def07588429033c891
vabene1111 <vabene1234@googlemail.com>
2021-09-13 12:39:29 +0200

    basics

---
+++ vue/src/apps/RecipeEditView/RecipeEditView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app" >
+
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import {BootstrapVue} from 'bootstrap-vue'
+
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import {ResolveUrlMixin} from "@/utils/utils";
+
+Vue.use(BootstrapVue)
+
+export default {
+  name: 'RecipeSearchView',
+  mixins: [ResolveUrlMixin],
+  components: {},
+  data() {
+    return {
+
+    }
+
+  },
+  computed: {
+
+  },
+  mounted() {
+
+    this.$i18n.locale = window.CUSTOM_LOCALE
+  },
+  watch: {
+
+  },
+  methods: {
+
+  }
+}
+
+</script>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
+
+<style>
+
+</style>
