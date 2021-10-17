bfbf932f6bf32f38af6a69c02a5cce29401a81d8
Kaibu <notkaibu@gmail.com>
2021-10-17 17:31:52 +0200

    setting starting day in meal plan

--- vue/src/components/MealPlanCalenderHeader.vue
+++ vue/src/components/MealPlanCalenderHeader.vue
@@ -2,50 +2,51 @@
   <div class="cv-header">
     <div class="cv-header-nav">
       <button
-          :disabled="!headerProps.previousYear"
-          class="previousYear"
-          aria-label="Previous Year"
-          @click.prevent="onInput(headerProps.previousYear)"
-      >
-        {{ previousYearLabel }}
-      </button>
-      <button
           :disabled="!headerProps.previousPeriod"
           class="previousPeriod"
           aria-label="Previous Period"
           @click.prevent="onInput(headerProps.previousPeriod)"
-          v-html="previousPeriodLabel"
-      />
+          v-b-tooltip.hover.top
+          :title="$t('Previous_Period')"
+          v-html="previousYearLabel"/>
+      <button
+          class="previousDay"
+          aria-label="Previous Day"
+          @click.prevent="onDayBack"
+          v-b-tooltip.hover.top
+          :title="$t('Previous_Day')"
+          v-html="previousPeriodLabel"/>
       <button
           class="currentPeriod"
           aria-label="Current Period"
-          @click.prevent="onInput(headerProps.currentPeriod)"
-      >
+          @click.prevent="onInput(headerProps.currentPeriod)">
         {{ headerProps.currentPeriodLabel }}
       </button>
+      <button
+          class="nextDay"
+          aria-label="Next Day"
+          @click.prevent="onDayForward"
+          v-b-tooltip.hover.top
+          :title="$t('Next_Day')"
+          v-html="nextPeriodLabel"/>
       <button
           :disabled="!headerProps.nextPeriod"
           class="nextPeriod"
           aria-label="Next Period"
+          v-b-tooltip.hover.top
+          :title="$t('Next_Period')"
           @click.prevent="onInput(headerProps.nextPeriod)"
-      >
-        {{ nextPeriodLabel }}
-      </button>
-      <button
-          :disabled="!headerProps.nextYear"
-          class="nextYear"
-          aria-label="Next Year"
-          @click.prevent="onInput(headerProps.nextYear)">
-        {{ nextYearLabel }}
-      </button>
+          v-html="nextYearLabel"/>
     </div>
     <div class="periodLabel">
       <slot name="label">{{ headerProps.periodLabel }}</slot>
     </div>
     <div class="actionArea d-none d-sm-flex">
-      <button class="btn btn-success plus-button pt-1 pb-1" @click="$emit('create-new')"><i class="fas fa-plus"></i></button>
+      <button class="btn btn-success plus-button pt-1 pb-1" @click="$emit('create-new')"><i class="fas fa-plus"></i>
+      </button>
       <span class="delete-area text-danger p-1 mr-2 ml-2" @drop.prevent="onDeleteDrop($event)"
-            @dragenter.prevent="onDeleteDragEnter($event)" @dragleave.prevent="onDeleteDragLeave($event)" @dragover.prevent="onDeleteDragEnter"><i
+            @dragenter.prevent="onDeleteDragEnter($event)" @dragleave.prevent="onDeleteDragLeave($event)"
+            @dragover.prevent="onDeleteDragEnter"><i
           class="fas fa-trash"></i> {{ $t('Drag_Here_To_Delete') }}</span>
     </div>
   </div>
@@ -64,6 +65,12 @@ export default {
     nextYearLabel: {type: String, default: ">>"},
   },
   methods: {
+    onDayForward() {
+      this.$emit("set-starting-day-forward")
+    },
+    onDayBack() {
+      this.$emit("set-starting-day-back")
+    },
     onInput(d) {
       this.$emit("input", d)
     },
