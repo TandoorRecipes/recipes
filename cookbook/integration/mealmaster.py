4021bbe672d0efe43e43a375f90de32755a55f03
vabene1111 <vabene1234@googlemail.com>
2021-11-10 08:45:13 +0100

    fixed plan to eat and mealmaster imports

--- cookbook/integration/mealmaster.py
+++ cookbook/integration/mealmaster.py
@@ -63,7 +63,7 @@ class MealMaster(Integration):
         current_recipe = ''
 
         for fl in file.readlines():
-            line = fl.decode("ANSI")
+            line = fl.decode("windows-1250")
             if (line.startswith('MMMMM') or line.startswith('-----')) and 'meal-master' in line.lower():
                 if current_recipe != '':
                     recipe_list.append(current_recipe)
