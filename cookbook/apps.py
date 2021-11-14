e762dc1c99e92b496df1c81a6399e1860d206098
vabene1111 <vabene1234@googlemail.com>
2021-11-14 10:33:33 +0100

    fixed startup failing if fix tree fails

--- cookbook/apps.py
+++ cookbook/apps.py
@@ -1,7 +1,11 @@
+import traceback
+
 from django.apps import AppConfig
 from django.conf import settings
 from django.db import OperationalError, ProgrammingError
 from django_scopes import scopes_disabled
+
+from recipes.settings import DEBUG
 
 
 class CookbookConfig(AppConfig):
@@ -9,7 +13,8 @@ class CookbookConfig(AppConfig):
 
     def ready(self):
         # post_save signal is only necessary if using full-text search on postgres
-        if settings.DATABASES['default']['ENGINE'] in ['django.db.backends.postgresql_psycopg2', 'django.db.backends.postgresql']:
+        if settings.DATABASES['default']['ENGINE'] in ['django.db.backends.postgresql_psycopg2',
+                                                       'django.db.backends.postgresql']:
             import cookbook.signals  # noqa
 
         # when starting up run fix_tree to:
@@ -21,6 +26,14 @@ class CookbookConfig(AppConfig):
                 Keyword.fix_tree(fix_paths=True)
                 Food.fix_tree(fix_paths=True)
             except OperationalError:
+                if DEBUG:
+                    traceback.print_exc()
                 pass  # if model does not exist there is no need to fix it
             except ProgrammingError:
+                if DEBUG:
+                    traceback.print_exc()
                 pass  # if migration has not been run database cannot be fixed yet
+            except Exception:
+                if DEBUG:
+                    traceback.print_exc()
+                pass  # dont break startup just because fix could not run, need to investigate cases when this happens
