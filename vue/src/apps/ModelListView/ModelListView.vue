415814a73af3e03afdcb22f04fb082164d0a13b5
smilerz <smilerz@gmail.com>
2021-11-21 11:11:51 -0600

    yarn build

--- vue/src/apps/ModelListView/ModelListView.vue
+++ vue/src/apps/ModelListView/ModelListView.vue
@@ -54,71 +54,6 @@
                     </div>
                 </div>
             </div>
-          </div>
-
-          <div class="row">
-            <div class="col-md-9" style="margin-top: 1vh">
-              <h3>
-                <!-- <span><b-button variant="link" size="sm" class="text-dark shadow-none"><i class="fas fa-chevron-down"></i></b-button></span> -->
-                <model-menu/>
-                <span>{{ this.this_model.name }}</span>
-                <span v-if="this_model.name !== 'Step'"><b-button variant="link" @click="startAction({'action':'new'})"><i
-                    class="fas fa-plus-circle fa-2x"></i></b-button></span><!-- TODO add proper field to model config to determine if create should be available or not -->
-              </h3>
-            </div>
-            <div class="col-md-3" style="position: relative; margin-top: 1vh">
-              <b-form-checkbox v-model="show_split" name="check-button" v-if="paginated"
-                               class="shadow-none"
-                               style="position:relative;top: 50%;  transform: translateY(-50%);" switch>
-                {{ $t('show_split_screen') }}
-              </b-form-checkbox>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col" :class="{'col-md-6' : show_split}">
-              <!-- model isn't paginated and loads in one API call -->
-              <div v-if="!paginated">
-                <generic-horizontal-card v-for="i in items_left" v-bind:key="i.id"
-                                         :item=i
-                                         :model="this_model"
-                                         @item-action="startAction($event, 'left')"
-                                         @finish-action="finishAction"/>
-              </div>
-              <!-- model is paginated and needs managed -->
-              <generic-infinite-cards v-if="paginated"
-                                      :card_counts="left_counts"
-                                      :scroll="show_split"
-                                      @search="getItems($event, 'left')"
-                                      @reset="resetList('left')">
-                <template v-slot:cards>
-                  <generic-horizontal-card
-                      v-for="i in items_left" v-bind:key="i.id"
-                      :item=i
-                      :model="this_model"
-                      @item-action="startAction($event, 'left')"
-                      @finish-action="finishAction"/>
-                </template>
-              </generic-infinite-cards>
-            </div>
-            <div class="col col-md-6" v-if="show_split">
-              <generic-infinite-cards v-if="this_model"
-                                      :card_counts="right_counts"
-                                      :scroll="show_split"
-                                      @search="getItems($event, 'right')"
-                                      @reset="resetList('right')">
-                <template v-slot:cards>
-                  <generic-horizontal-card
-                      v-for="i in items_right" v-bind:key="i.id"
-                      :item=i
-                      :model="this_model"
-                      @item-action="startAction($event, 'right')"
-                      @finish-action="finishAction"/>
-                </template>
-              </generic-infinite-cards>
-            </div>
-
-          </div>
         </div>
     </div>
 </template>
@@ -129,7 +64,8 @@ import { BootstrapVue } from "bootstrap-vue"
 
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
-import { CardMixin, ApiMixin, getConfig, StandardToasts, getUserPreference, makeToast } from "@/utils/utils"
+import { CardMixin, ApiMixin, getConfig } from "@/utils/utils"
+import { StandardToasts, ToastMixin } from "@/utils/utils"
 
 import GenericInfiniteCards from "@/components/GenericInfiniteCards"
 import GenericHorizontalCard from "@/components/GenericHorizontalCard"
@@ -141,149 +77,33 @@ import { ApiApiFactory } from "@/utils/openapi/api"
 Vue.use(BootstrapVue)
 
 export default {
-  // TODO ApiGenerator doesn't capture and share error information - would be nice to share error details when available
-  // or i'm capturing it incorrectly
-  name: 'ModelListView',
-  mixins: [CardMixin, ApiMixin, ToastMixin],
-  components: {
-     GenericHorizontalCard, GenericModalForm, GenericInfiniteCards, ModelMenu,
-  },
-  data() {
-    return {
-      // this.Models and this.Actions inherited from ApiMixin
-      items_left: [],
-      items_right: [],
-      right_counts: {'max': 9999, 'current': 0},
-      left_counts: {'max': 9999, 'current': 0},
-      this_model: undefined,
-      model_menu: undefined,
-      this_action: undefined,
-      this_recipe_param: undefined,
-      this_item: {},
-      this_target: {},
-      show_modal: false,
-      show_split: false,
-      paginated: false,
-      header_component_name: undefined,
-    }
-  },
-  computed: {
-    headerComponent() {
-      // TODO this leads webpack to create one .js file for each component in this folder because at runtime any one of them could be requested
-      // TODO this is not necessarily bad but maybe there are better options to do this
-      return () => import(/* webpackChunkName: "header-component" */ `@/components/${this.header_component_name}`)
-    }
-  },
-  mounted() {
-    // value is passed from lists.py
-    let model_config = JSON.parse(document.getElementById('model_config').textContent)
-    this.this_model = this.Models[model_config?.model]
-    this.this_recipe_param = model_config?.recipe_param
-    this.paginated = this.this_model?.paginated ?? false
-    this.header_component_name = this.this_model?.list?.header_component?.name ?? undefined
-    this.$nextTick(() => {
-      if (!this.paginated) {
-        this.getItems({page:1},'left')
-      }
-    })
-    this.$i18n.locale = window.CUSTOM_LOCALE
-  },
-  methods: {
-    // this.genericAPI inherited from ApiMixin
-    resetList: function (e) {
-      this['items_' + e] = []
-      this[e + '_counts'].max = 9999 + Math.random()
-      this[e + '_counts'].current = 0
+    // TODO ApiGenerator doesn't capture and share error information - would be nice to share error details when available
+    // or i'm capturing it incorrectly
+    name: "ModelListView",
+    mixins: [CardMixin, ApiMixin, ToastMixin],
+    components: {
+        GenericHorizontalCard,
+        GenericModalForm,
+        GenericInfiniteCards,
+        ModelMenu,
     },
-    startAction: function (e, param) {
-      let source = e?.source ?? {}
-      let target = e?.target ?? undefined
-      this.this_item = source
-      this.this_target = target
-
-      switch (e.action) {
-        case 'delete':
-          this.this_action = this.Actions.DELETE
-          this.show_modal = true
-          break;
-        case 'new':
-          this.this_action = this.Actions.CREATE
-          this.show_modal = true
-          break;
-        case 'edit':
-          this.this_item = e.source
-          this.this_action = this.Actions.UPDATE
-          this.show_modal = true
-          break;
-        case 'move':
-          if (target == null) {
-            this.this_item = e.source
-            this.this_action = this.Actions.MOVE
-            this.show_modal = true
-          } else {
-            this.moveThis(source.id, target.id)
-          }
-          break;
-        case 'merge':
-          if (target == null) {
-            this.this_item = e.source
-            this.this_action = this.Actions.MERGE
-            this.show_modal = true
-          } else {
-            this.mergeThis(e.source, e.target, false)
-          }
-          break;
-        case 'merge-automate':
-          if (target == null) {
-            this.this_item = e.source
-            this.this_action = this.Actions.MERGE
-            this.show_modal = true
-          } else {
-            this.mergeThis(e.source, e.target, true)
-          }
-          break
-        case 'get-children':
-          if (source.show_children) {
-            Vue.set(source, 'show_children', false)
-          } else {
-            this.getChildren(param, source)
-          }
-          break;
-        case 'get-recipes':
-          if (source.show_recipes) {
-            Vue.set(source, 'show_recipes', false)
-          } else {
-            this.getRecipes(param, source)
-          }
-          break;
-      }
-    },
-    finishAction: function (e) {
-      let update = undefined
-      switch (e?.action) {
-        case 'save':
-          this.saveThis(e.form_data)
-          break;
-      }
-      if (e !== 'cancel') {
-        switch (this.this_action) {
-          case this.Actions.DELETE:
-            this.deleteThis(this.this_item.id)
-            break;
-          case this.Actions.CREATE:
-            this.saveThis(e.form_data)
-            break;
-          case this.Actions.UPDATE:
-            update = e.form_data
-            update.id = this.this_item.id
-            this.saveThis(update)
-            break;
-          case this.Actions.MERGE:
-            this.mergeThis(this.this_item, e.form_data.target, false)
-            break;
-          case this.Actions.MOVE:
-            this.moveThis(this.this_item.id, e.form_data.target.id)
-            break;
+    data() {
+        return {
+            // this.Models and this.Actions inherited from ApiMixin
+            items_left: [],
+            items_right: [],
+            right_counts: { max: 9999, current: 0 },
+            left_counts: { max: 9999, current: 0 },
+            this_model: undefined,
+            model_menu: undefined,
+            this_action: undefined,
+            this_recipe_param: undefined,
+            this_item: {},
+            this_target: {},
+            show_modal: false,
+            show_split: false,
+            paginated: false,
+            header_component_name: undefined,
         }
     },
     computed: {
@@ -379,13 +199,6 @@ export default {
                         this.getRecipes(param, source)
                     }
                     break
-                case "add-shopping":
-                    //TODO: add modal to edit units and amount
-                    this.addShopping(e.source)
-                    break
-                case "add-onhand":
-                    this.addOnhand(e.source)
-                    break
             }
         },
         finishAction: function (e) {
@@ -425,6 +238,26 @@ export default {
                     let results = result.data?.results ?? result.data
 
                     if (results?.length) {
+                        // let secondaryRequest = undefined;
+                        // if (this['items_' + column]?.length < getConfig(this.this_model, this.Actions.LIST).config.pageSize.default * (params.page - 1)) {
+                        //   // the item list is smaller than it should be based on the site the user is own
+                        //   // this happens when an item is deleted (or merged)
+                        //   // to prevent issues insert the last item of the previous search page before loading the new results
+                        //   params.page = params.page - 1
+                        //   secondaryRequest = this.genericAPI(this.this_model, this.Actions.LIST, params).then((result) => {
+                        //     let prev_page_results = result.data?.results ?? result.data
+                        //     if (prev_page_results?.length) {
+                        //       results = [prev_page_results[prev_page_results.length]].concat(results)
+                        //
+                        //       this['items_' + column] = this['items_' + column].concat(results) //TODO duplicate code, find some elegant workaround
+                        //       this[column + '_counts']['current'] = getConfig(this.this_model, this.Actions.LIST).config.pageSize.default * (params.page - 1) + results.length
+                        //       this[column + '_counts']['max'] = result.data?.count ?? 0
+                        //     }
+                        //   })
+                        // } else {
+                        //
+                        // }
+
                         this["items_" + column] = this["items_" + column].concat(results)
                         this[column + "_counts"]["current"] = getConfig(this.this_model, this.Actions.LIST).config.pageSize.default * (params.page - 1) + results.length
                         this[column + "_counts"]["max"] = result.data?.count ?? 0
@@ -484,11 +317,11 @@ export default {
                 .then((result) => {
                     this.moveUpdateItem(source_id, target_id)
                     // TODO make standard toast
-                    makeToast(this.$t("Success"), "Succesfully moved resource", "success")
+                    StandardToasts.makeStandardToast(StandardToasts.SUCCESS_MOVE)
                 })
                 .catch((err) => {
                     console.log(err)
-                    StandardToasts.makeStandardToast(StandardToasts.FAIL_MOVE, err?.bodyText)
+                    this.makeToast(this.$t("Error"), err.bodyText, "danger")
                 })
         },
         moveUpdateItem: function (source_id, target_id) {
@@ -529,7 +362,7 @@ export default {
                 .catch((err) => {
                     //TODO error checking not working with OpenAPI methods
                     console.log("Error", err)
-                    StandardToasts.makeStandardToast(StandardToasts.FAIL_MOVE, err?.bodyText)
+                    this.makeToast(this.$t("Error"), err.bodyText, "danger")
                 })
 
             if (automate) {
@@ -578,7 +411,7 @@ export default {
                 })
                 .catch((err) => {
                     console.log(err)
-                    makeToast(this.$t("Error"), err.bodyText, "danger")
+                    StandardToasts.makeStandardToast(StandardToasts.FAIL_FETCH)
                 })
         },
         getRecipes: function (col, item) {
@@ -598,7 +431,7 @@ export default {
                 })
                 .catch((err) => {
                     console.log(err)
-                    makeToast(this.$t("Error"), err.bodyText, "danger")
+                    this.makeToast(this.$t("Error"), err.bodyText, "danger")
                 })
         },
         refreshThis: function (id) {
@@ -613,7 +446,7 @@ export default {
         },
         clearState: function () {
             this.show_modal = false
-            this.this_action = {}
+            this.this_action = undefined
             this.this_item = undefined
             this.this_target = undefined
         },
