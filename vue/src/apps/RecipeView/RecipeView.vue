2ea85d2d84c404fb91a842f089117e59d15626fc
smilerz <smilerz@gmail.com>
2021-11-21 10:59:11 -0600

    Fix after rebase

--- vue/src/apps/RecipeView/RecipeView.vue
+++ vue/src/apps/RecipeView/RecipeView.vue
@@ -237,7 +237,6 @@ export default {
         updateStartTime: function (e) {
             this.start_time = e
         },
-
         updateIngredientCheckedState: function (e) {
             for (let step of this.recipe.steps) {
                 for (let ingredient of step.ingredients) {
