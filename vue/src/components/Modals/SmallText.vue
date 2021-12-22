c20e036d90e9debd06975f9186412772e268d21c
smilerz <smilerz@gmail.com>
2021-12-22 16:18:45 -0600

    add path to generic cards for food/keywords

---
+++ vue/src/components/Modals/SmallText.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="small text-muted">
+        {{ value }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TextInput",
+    props: {
+        value: { type: String, default: "" },
+    },
+    data() {
+        return {}
+    },
+    mounted() {},
+    watch: {},
+    methods: {},
+}
+</script>
