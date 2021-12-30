582e145a9f96016a6eb4b6a2f2e7219b13e501a7
smilerz <smilerz@gmail.com>
2021-12-30 13:55:38 -0600

    Fix after rebase

--- cookbook/apps.py
+++ cookbook/apps.py
@@ -14,24 +14,24 @@ class CookbookConfig(AppConfig):
     def ready(self):
         import cookbook.signals  # noqa
 
-        if not settings.DISABLE_TREE_FIX_STARTUP:
-            # when starting up run fix_tree to:
-            #   a) make sure that nodes are sorted when switching between sort modes
-            #   b) fix problems, if any, with tree consistency
-            with scopes_disabled():
-                try:
-                    from cookbook.models import Food, Keyword
-                    Keyword.fix_tree(fix_paths=True)
-                    Food.fix_tree(fix_paths=True)
-                except OperationalError:
-                    if DEBUG:
-                        traceback.print_exc()
-                    pass  # if model does not exist there is no need to fix it
-                except ProgrammingError:
-                    if DEBUG:
-                        traceback.print_exc()
-                    pass  # if migration has not been run database cannot be fixed yet
-                except Exception:
-                    if DEBUG:
-                        traceback.print_exc()
-                    pass  # dont break startup just because fix could not run, need to investigate cases when this happens
+        # if not settings.DISABLE_TREE_FIX_STARTUP:
+        #     # when starting up run fix_tree to:
+        #     #   a) make sure that nodes are sorted when switching between sort modes
+        #     #   b) fix problems, if any, with tree consistency
+        #     with scopes_disabled():
+        #         try:
+        #             from cookbook.models import Food, Keyword
+        #             Keyword.fix_tree(fix_paths=True)
+        #             Food.fix_tree(fix_paths=True)
+        #         except OperationalError:
+        #             if DEBUG:
+        #                 traceback.print_exc()
+        #             pass  # if model does not exist there is no need to fix it
+        #         except ProgrammingError:
+        #             if DEBUG:
+        #                 traceback.print_exc()
+        #             pass  # if migration has not been run database cannot be fixed yet
+        #         except Exception:
+        #             if DEBUG:
+        #                 traceback.print_exc()
+        #             pass  # dont break startup just because fix could not run, need to investigate cases when this happens
