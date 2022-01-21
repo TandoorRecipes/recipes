1fe8a04b60dfebc026e5f2087473082c36c309b1
vabene1111 <vabene1234@googlemail.com>
2022-01-21 17:49:27 +0100

    added keyword clicking to recipe view and fixed deleted keyword showing in search when passed via parameter

--- vue/src/components/KeywordsComponent.vue
+++ vue/src/components/KeywordsComponent.vue
@@ -1,17 +1,22 @@
 <template>
     <div v-if="recipe.keywords.length > 0">
       <span :key="k.id" v-for="k in recipe.keywords" class="pl-1">
-        <b-badge pill variant="light" class="font-weight-normal">{{k.label}}</b-badge>
+          <a :href="`${resolveDjangoUrl('view_search')}?keyword=${k.id}`"><b-badge pill variant="light"
+                                                                                   class="font-weight-normal">{{ k.label }}</b-badge></a>
+
       </span>
     </div>
 </template>
 
 <script>
 
+import {ResolveUrlMixin} from "@/utils/utils";
+
 export default {
-  name: 'KeywordsComponent',
-  props: {
-    recipe: Object,
-  },
+    name: 'KeywordsComponent',
+    mixins: [ResolveUrlMixin],
+    props: {
+        recipe: Object,
+    },
 }
 </script>
