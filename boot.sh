45942dfa7f9a8f81948364dcab546990f0cc77e5
MaxJa4 <74194322+MaxJa4@users.noreply.github.com>
2022-02-08 18:20:19 +0100

    Fixed indentation

--- boot.sh
+++ boot.sh
@@ -12,7 +12,7 @@ done
 if [ $attempt -gt $max_attempts ]; then
     echo -e "\nDatabase not reachable. Maximum attempts exceeded."
     echo "Please check logs above - misconfiguration is very likely."
-	echo "Make sure the DB container is up and POSTGRES_HOST is set properly."
+    echo "Make sure the DB container is up and POSTGRES_HOST is set properly."
     echo "Shutting down container."
     exit 1 # exit with error to make the container stop
 fi
