96bd66f9e6646ae6706a5effdab814c640308128
vabene1111 <vabene1234@googlemail.com>
2022-03-15 11:48:12 +0100

    fixed telegram bot

--- cookbook/views/telegram.py
+++ cookbook/views/telegram.py
@@ -1,4 +1,5 @@
 import json
+import traceback
 
 import requests
 from django.db.models import Q
@@ -52,10 +53,10 @@ def hook(request, token):
             f = ingredient_parser.get_food(food)
             u = ingredient_parser.get_unit(unit)
 
-            ShoppingListEntry.objects.create(food=f, unit=u, amount=amount, original_text=ingredient, created_by=request.user, space=request.space)
+            ShoppingListEntry.objects.create(food=f, unit=u, amount=amount, original_text=data['message']['text'], created_by=request.user, space=request.space)
 
             return JsonResponse({'data': data['message']['text']})
     except Exception:
-        pass
+        traceback.print_exc()
 
     return JsonResponse({})
