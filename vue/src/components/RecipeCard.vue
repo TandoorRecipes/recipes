82c2cc0f4076806cec79fcbe9faa1c4c748b76f9
Kaibu <notkaibu@gmail.com>
2022-04-04 22:53:26 +0200

    ui improvements

--- vue/src/components/RecipeCard.vue
+++ vue/src/components/RecipeCard.vue
@@ -2,7 +2,7 @@
     <b-card no-body v-hover v-if="recipe">
         <a :href="clickUrl()">
             <b-card-img-lazy style="height: 15vh; object-fit: cover" class="" :src="recipe_image" v-bind:alt="$t('Recipe_Image')" top></b-card-img-lazy>
-            <div class="card-img-overlay h-100 d-flex flex-column justify-content-right float-right text-right pt-2 pr-1">
+            <div class="card-img-overlay h-100 d-flex flex-column justify-content-right float-right text-right pt-2 pr-1" v-if="show_context_menu">
                 <a>
                     <recipe-context-menu :recipe="recipe" class="float-right" v-if="recipe !== null"></recipe-context-menu>
                 </a>
@@ -78,6 +78,7 @@ export default {
         footer_text: String,
         footer_icon: String,
         detailed: { type: Boolean, default: true },
+        show_context_menu: { type: Boolean, default: true }
     },
     mounted() {},
     computed: {
