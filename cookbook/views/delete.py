cb02490c4d97ce7f2f4b7413d8b67e41ce16162c
vabene1111 <vabene1234@googlemail.com>
2022-05-31 20:38:53 +0200

    join and leave spaces

--- cookbook/views/delete.py
+++ cookbook/views/delete.py
@@ -9,7 +9,7 @@ from django.views.generic import DeleteView
 
 from cookbook.helper.permission_helper import GroupRequiredMixin, OwnerRequiredMixin, group_required
 from cookbook.models import (Comment, InviteLink, MealPlan, Recipe, RecipeBook, RecipeBookEntry,
-                             RecipeImport, Storage, Sync)
+                             RecipeImport, Storage, Sync, UserSpace)
 from cookbook.provider.dropbox import Dropbox
 from cookbook.provider.local import Local
 from cookbook.provider.nextcloud import Nextcloud
@@ -187,4 +187,15 @@ class InviteLinkDelete(OwnerRequiredMixin, DeleteView):
     def get_context_data(self, **kwargs):
         context = super(InviteLinkDelete, self).get_context_data(**kwargs)
         context['title'] = _("Invite Link")
+        return context
+
+
+class UserSpaceDelete(OwnerRequiredMixin, DeleteView):
+    template_name = "generic/delete_template.html"
+    model = UserSpace
+    success_url = reverse_lazy('view_space_overview')
+
+    def get_context_data(self, **kwargs):
+        context = super(UserSpaceDelete, self).get_context_data(**kwargs)
+        context['title'] = _("Space Membership")
         return context
