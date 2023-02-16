9573ff0932a235156f99e590f15488999d73a770
vabene1111 <vabene1234@googlemail.com>
2023-02-16 18:26:26 +0100

    setup test view

--- vue/src/apps/TestView/TestView.vue
+++ vue/src/apps/TestView/TestView.vue
@@ -2,37 +2,50 @@
 
     <div id="app">
 
+        Recipe Card
+        <b-checkbox v-model="recipe_detailed"></b-checkbox>
+        <div class="row">
+            <div class="col-md-3">
+                <recipe-card
+                    v-if="recipe !== undefined"
+                    :recipe="recipe"
+                    :detailed="recipe_detailed"
+                ></recipe-card>
+            </div>
+        </div>
+
     </div>
 </template>
 
 <script>
 import Vue from "vue"
-import { BootstrapVue } from "bootstrap-vue"
+import {BootstrapVue} from "bootstrap-vue"
 
 import "bootstrap-vue/dist/bootstrap-vue.css"
+import {ApiApiFactory} from "@/utils/openapi/api";
+import RecipeCard from "@/components/RecipeCard.vue";
 
 Vue.use(BootstrapVue)
 
-//import Multiselect from "vue-multiselect"
 
 export default {
     name: "TestView",
     mixins: [],
-    components: {
-      // Multiselect,
-    },
+    components: {RecipeCard},
     data() {
         return {
-
+            recipe: undefined,
+            recipe_detailed: false,
         }
     },
     mounted() {
         this.$i18n.locale = window.CUSTOM_LOCALE
-
+        let apiClient = new ApiApiFactory()
+        apiClient.retrieveRecipe('119').then((r) => {
+            this.recipe = r.data
+        })
     },
-    methods: {
-
-    },
+    methods: {},
 }
 </script>
 
