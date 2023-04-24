a7a6abe3d22d06926bee04457efe5c567472ae44
smilerz <smilerz@gmail.com>
2023-04-24 10:59:48 -0500

    add NEVER_UNIT automation

--- cookbook/models.py
+++ cookbook/models.py
@@ -770,7 +770,8 @@ class PropertyType(models.Model, PermissionModelMixin):
     icon = models.CharField(max_length=16, blank=True, null=True)
     order = models.IntegerField(default=0)
     description = models.CharField(max_length=512, blank=True, null=True)
-    category = models.CharField(max_length=64, choices=((NUTRITION, _('Nutrition')), (ALLERGEN, _('Allergen')), (PRICE, _('Price')), (GOAL, _('Goal')), (OTHER, _('Other'))), null=True, blank=True)
+    category = models.CharField(max_length=64, choices=((NUTRITION, _('Nutrition')), (ALLERGEN, _('Allergen')),
+                                (PRICE, _('Price')), (GOAL, _('Goal')), (OTHER, _('Other'))), null=True, blank=True)
     open_data_slug = models.CharField(max_length=128, null=True, blank=True, default=None)
 
     # TODO show if empty property?
