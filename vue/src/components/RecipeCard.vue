d0481ed18ccd7647ed577cf50380c807982616be
vabene1111 <vabene1234@googlemail.com>
2023-11-27 19:59:06 +0100

    fixed recipe card description overlay

--- vue/src/components/RecipeCard.vue
+++ vue/src/components/RecipeCard.vue
@@ -234,7 +234,7 @@ export default {
 .content .content-overlay {
     background: rgba(0, 0, 0, 0.7);
     position: absolute;
-    height: 99%;
+    height: 100%;
     width: 100%;
     left: 0;
     top: 0;
