ccedf1565b757c5b32a5925f9dd86ea5c8b49b1f
vabene1111 <vabene1234@googlemail.com>
2023-12-02 18:02:22 +0100

    fully integrated property editor

--- vue/src/components/RecipeContextMenu.vue
+++ vue/src/components/RecipeContextMenu.vue
@@ -10,6 +10,9 @@
                 <a class="dropdown-item" :href="resolveDjangoUrl('edit_recipe', recipe.id)" v-if="!disabled_options.edit"><i
                     class="fas fa-pencil-alt fa-fw"></i> {{ $t("Edit") }}</a>
 
+                <a class="dropdown-item" :href="resolveDjangoUrl('view_property_editor', recipe.id)" v-if="!disabled_options.edit">
+                    <i class="fas fa-table"></i> {{ $t("Property_Editor") }}</a>
+
                 <a class="dropdown-item" :href="resolveDjangoUrl('edit_convert_recipe', recipe.id)"
                    v-if="!recipe.internal && !disabled_options.convert"><i class="fas fa-exchange-alt fa-fw"></i> {{ $t("convert_internal") }}</a>
 
