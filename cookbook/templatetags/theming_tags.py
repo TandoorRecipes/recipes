3d8b1d6ccb39e8b1645d46b2942690e36ea31dc5
vabene1111 <vabene1234@googlemail.com>
2024-01-01 22:14:01 +0100

    lots of theming related changes
    - upload a custom logo for your space
        - space settings can override user settings for theming
        - spaces can upload custom CSS overrides
        - allow users to disable showing the tandoor/space logo
        - allow changing navigation background color to any color desired
        - allow switching navigation text color between dark/light (different effects depending on theme)

--- cookbook/templatetags/theming_tags.py
+++ cookbook/templatetags/theming_tags.py
@@ -1,7 +1,8 @@
 from django import template
 from django.templatetags.static import static
+from django_scopes import scopes_disabled
 
-from cookbook.models import UserPreference
+from cookbook.models import UserPreference, UserFile, Space
 from recipes.settings import STICKY_NAV_PREF_DEFAULT
 
 register = template.Library()
@@ -19,35 +20,58 @@ def theme_url(request):
         UserPreference.TANDOOR: 'themes/tandoor.min.css',
         UserPreference.TANDOOR_DARK: 'themes/tandoor_dark.min.css',
     }
-    if request.user.userpreference.theme in themes:
-        return static(themes[request.user.userpreference.theme])
+    # if request.space.custom_space_theme:
+    #     return request.space.custom_space_theme.file.url
+
+    if request.space.space_theme in themes:
+        return static(themes[request.space.space_theme])
     else:
-        raise AttributeError
+        if request.user.userpreference.theme in themes:
+            return static(themes[request.user.userpreference.theme])
+        else:
+            raise AttributeError
+
+
+@register.simple_tag
+def custom_theme(request):
+    if request.user.is_authenticated and request.space.custom_space_theme:
+        return request.space.custom_space_theme.file.url
 
 
 @register.simple_tag
 def logo_url(request):
-    if request.user.is_authenticated and getattr(getattr(request, "space", {}), 'image', None):
-        return request.space.image.file.url
+    if request.user.is_authenticated and getattr(getattr(request, "space", {}), 'nav_logo', None):
+        return request.space.nav_logo.file.url
     else:
         return static('assets/brand_logo.png')
 
 
 @register.simple_tag
-def nav_color(request):
+def nav_bg_color(request):
     if not request.user.is_authenticated:
-        return 'navbar-light bg-primary'
-
-    if request.user.userpreference.nav_color.lower() in ['light', 'warning', 'info', 'success']:
-        return f'navbar-light bg-{request.user.userpreference.nav_color.lower()}'
+        return '#ddbf86'
     else:
-        return f'navbar-dark bg-{request.user.userpreference.nav_color.lower()}'
+        if request.space.nav_bg_color:
+            return request.space.nav_bg_color
+        else:
+            return request.user.userpreference.nav_bg_color
+
+
+@register.simple_tag
+def nav_text_color(request):
+    type_mapping = {Space.DARK: 'navbar-light', Space.LIGHT: 'navbar-dark'}  # inverted since navbar-dark means the background
+    if not request.user.is_authenticated:
+        return 'navbar-dark'
+    else:
+        if request.space.nav_text_color != Space.BLANK:
+            return type_mapping[request.space.nav_text_color]
+        else:
+            return type_mapping[request.user.userpreference.nav_text_color]
 
 
 @register.simple_tag
 def sticky_nav(request):
-    if (not request.user.is_authenticated and STICKY_NAV_PREF_DEFAULT) or \
-            (request.user.is_authenticated and request.user.userpreference.sticky_navbar):  # noqa: E501
+    if (not request.user.is_authenticated and STICKY_NAV_PREF_DEFAULT) or (request.user.is_authenticated and request.user.userpreference.nav_sticky):
         return 'position: sticky; top: 0; left: 0; z-index: 1000;'
     else:
         return ''
