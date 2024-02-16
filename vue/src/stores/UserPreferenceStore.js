48447f1c8a63fd588e09b7f44d7a50adafd397ab
vabene1111 <vabene1234@googlemail.com>
2024-02-16 20:40:40 +0100

    fixed always false check

--- vue/src/stores/UserPreferenceStore.js
+++ vue/src/stores/UserPreferenceStore.js
@@ -73,7 +73,7 @@ export const useUserPreferenceStore = defineStore(_STORE_ID, {
          */
         loadDeviceSettings() {
             let s = localStorage.getItem(_LS_DEVICE_SETTINGS)
-            if (!(s === null || s === {})) {
+            if (s !== null) {
                 let settings = JSON.parse(s)
                 for (s in settings) {
                     Vue.set(this.device_settings, s, settings[s])
