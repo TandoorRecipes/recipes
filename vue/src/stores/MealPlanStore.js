21094eecc6c1d60ba79cba7449c1953c994a5f9a
vabene1111 <vabene1234@googlemail.com>
2024-02-19 20:04:31 +0100

    fixed meal plan not loading

--- vue/src/stores/MealPlanStore.js
+++ vue/src/stores/MealPlanStore.js
@@ -47,7 +47,7 @@ export const useMealPlanStore = defineStore(_STORE_ID, {
     },
     actions: {
         refreshFromAPI(from_date, to_date) {
-            if (this.currently_updating != null && (this.currently_updating[0] !== from_date || this.currently_updating[1] !== to_date)) {
+            if (this.currently_updating == null || (this.currently_updating[0] !== from_date || this.currently_updating[1] !== to_date)) {
                 this.currently_updating = [from_date, to_date] // certainly no perfect check but better than nothing
 
                 let apiClient = new ApiApiFactory()
