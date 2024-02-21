257e886d8760947d68c719b8df10ca5ac81eeb30
vabene1111 <vabene1234@googlemail.com>
2024-02-21 18:09:51 +0100

    more basics

--- vue3/src/apps/tandoor/main.ts
+++ vue3/src/apps/tandoor/main.ts
@@ -7,31 +7,20 @@ import App from './Tandoor.vue'
 
 import 'vite/modulepreload-polyfill';
 import vuetify from "@/vuetify";
-import test1 from "@/components/test1.vue";
-import test2 from "@/components/test2.vue";
+import ShoppingListPage from "@/pages/ShoppingListPage.vue";
+import RecipeSearchPage from "@/pages/RecipeSearchPage.vue";
 
-// 1. Define route components.
-// These can be imported from other files
-
-
-// 2. Define some routes
-// Each route should map to a component.
-// We'll talk about nested routes later.
 const routes = [
-    {path: '/', component: test1},
-    {path: '/about', component: test2},
+    {path: '/', component: RecipeSearchPage},
+    {path: '/about', component: ShoppingListPage},
 ]
 
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
 const router = createRouter({
     // 4. Provide the history implementation to use. We
     // are using the hash history for simplicity here.
     history: createWebHashHistory(),
-    routes, // short for `routes: routes`
+    routes,
 })
-
 
 const app = createApp(App)
 
--- vue3/src/pages/ShoppingListPage.vue
+++ vue3/src/pages/ShoppingListPage.vue
@@ -1,14 +1,14 @@
+<template>
+    <h2>Shopping List</h2>
+</template>
+
 <script lang="ts">
 import {defineComponent} from 'vue'
 
 export default defineComponent({
-    name: "test2"
+    name: "ShoppingListPage"
 })
 </script>
-
-<template>
-About
-</template>
 
 <style scoped>
 
