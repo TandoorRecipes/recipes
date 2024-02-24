25887c0595e943cd5dce78b0317194f65b4c74d0
vabene1111 <vabene1234@googlemail.com>
2024-02-24 12:16:24 +0100

    steps overview in recipe view

---
+++ vue3/src/components/display/IngredientsTable.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-table density="compact" v-if="ingredients.length > 0">
+        <tbody>
+        <IngredientsTableRow v-for="i in ingredients" :ingredient="i" :key="i.id"></IngredientsTableRow>
+        </tbody>
+    </v-table>
+</template>
+
+<script lang="ts">
+import {defineComponent, PropType} from 'vue'
+import {Ingredient, Step} from "@/openapi";
+import IngredientsTableRow from "@/components/display/IngredientsTableRow.vue";
+
+export default defineComponent({
+    name: "IngredientsTable",
+    components: {IngredientsTableRow},
+    props: {
+        ingredients: {
+            type: [] as PropType<Array<Ingredient>>,
+            default: [],
+        },
+    },
+    mounted() {
+
+    }
+})
+</script>
+
+
+<style scoped>
+
+</style>
