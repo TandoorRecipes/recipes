a57b8f6081f8c644d8327bdcab41a15e07ef3d53
vabene1111 <vabene1234@googlemail.com>
2024-02-29 21:28:44 +0100

    start page

--- vue3/src/apps/tandoor/Tandoor.vue
+++ vue3/src/apps/tandoor/Tandoor.vue
@@ -2,44 +2,43 @@
     <v-app>
 
 
-            <v-app-bar color="tandoor" flat density="comfortable" >
-                <v-img class="ms-2" src="../../assets/brand_logo.svg"></v-img>
-                <global-search-dialog></global-search-dialog>
-                <v-spacer></v-spacer>
-                <v-btn density="compact" icon="fas fa-ellipsis-v"></v-btn>
-            </v-app-bar>
+        <v-app-bar color="tandoor" flat density="comfortable">
+            <v-img class="ms-2" src="../../assets/brand_logo.svg"></v-img>
+            <global-search-dialog></global-search-dialog>
+            <v-spacer></v-spacer>
+            <v-btn density="compact" icon="fas fa-ellipsis-v"></v-btn>
+        </v-app-bar>
 
 
-            <v-main>
-                <v-container class="pa-0 ma-0 mb-2">
-                    <router-view></router-view>
-                </v-container>
-            </v-main>
+        <v-main>
+            <v-container class="pa-0 ma-0 mb-2">
+                <router-view></router-view>
+            </v-container>
+        </v-main>
 
-            <v-bottom-navigation>
-                <v-btn value="recent" to="/search">
-                    <v-icon icon="fas fa-book"/>
-                    <span>Recipes</span>
-                </v-btn>
+        <v-bottom-navigation>
+            <v-btn value="recent" to="/search">
+                <v-icon icon="fas fa-book"/>
+                <span>Recipes</span>
+            </v-btn>
 
-                <v-btn value="favorites" to="/mealplan">
-                    <v-icon icon="fas fa-calendar-alt"></v-icon>
+            <v-btn value="favorites" to="/mealplan">
+                <v-icon icon="fas fa-calendar-alt"></v-icon>
 
-                    <span>MealPlan</span>
-                </v-btn>
+                <span>MealPlan</span>
+            </v-btn>
 
-                <v-btn value="nearby" to="/shopping">
-                    <v-icon icon="fas fa-shopping-cart"></v-icon>
+            <v-btn value="nearby" to="/shopping">
+                <v-icon icon="fas fa-shopping-cart"></v-icon>
 
-                    <span>Shopping</span>
-                </v-btn>
-                <v-btn value="nearby" to="/books">
-                    <v-icon icon="fas fa-book-open"></v-icon>
+                <span>Shopping</span>
+            </v-btn>
+            <v-btn value="nearby" to="/books">
+                <v-icon icon="fas fa-book-open"></v-icon>
 
-                    <span>Books</span>
-                </v-btn>
-            </v-bottom-navigation>
-
+                <span>Books</span>
+            </v-btn>
+        </v-bottom-navigation>
 
 
     </v-app>
