54ad7db2c18b0f1c703975dddaea6899bc2cff4e
smilerz <smilerz@gmail.com>
2024-03-27 08:20:26 -0500

    regenerate openapi

--- vue3/src/openapi/models/Automation.ts
+++ vue3/src/openapi/models/Automation.ts
@@ -13,12 +13,12 @@
  */
 
 import { mapValues } from '../runtime';
-import type { AutomationTypeEnum } from './AutomationTypeEnum';
+import type { TypeEnum } from './TypeEnum';
 import {
-    AutomationTypeEnumFromJSON,
-    AutomationTypeEnumFromJSONTyped,
-    AutomationTypeEnumToJSON,
-} from './AutomationTypeEnum';
+    TypeEnumFromJSON,
+    TypeEnumFromJSONTyped,
+    TypeEnumToJSON,
+} from './TypeEnum';
 
 /**
  * 
@@ -34,10 +34,10 @@ export interface Automation {
     readonly id: number;
     /**
      * 
-     * @type {AutomationTypeEnum}
+     * @type {TypeEnum}
      * @memberof Automation
      */
-    type: AutomationTypeEnum;
+    type: TypeEnum;
     /**
      * 
      * @type {string}
@@ -109,7 +109,7 @@ export function AutomationFromJSONTyped(json: any, ignoreDiscriminator: boolean)
     return {
         
         'id': json['id'],
-        'type': AutomationTypeEnumFromJSON(json['type']),
+        'type': TypeEnumFromJSON(json['type']),
         'name': json['name'] == null ? undefined : json['name'],
         'description': json['description'] == null ? undefined : json['description'],
         'param1': json['param_1'] == null ? undefined : json['param_1'],
@@ -127,7 +127,7 @@ export function AutomationToJSON(value?: Automation | null): any {
     }
     return {
         
-        'type': AutomationTypeEnumToJSON(value['type']),
+        'type': TypeEnumToJSON(value['type']),
         'name': value['name'],
         'description': value['description'],
         'param_1': value['param1'],
