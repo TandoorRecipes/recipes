f560365dedf40cac556f191f156406dee2379c15
smilerz <smilerz@gmail.com>
2024-04-05 08:15:01 -0500

    stub out schema completeness tests

--- cookbook/tests/other/test_schemas.py
+++ cookbook/tests/other/test_schemas.py
@@ -15,5 +15,29 @@ def test_pagination_exists(route):
             or 'get' in route[1].http_method_names and hasattr(route[1], 'pagination_class') and route[1].pagination_class is not None), f"API {route[0]} is not paginated."
 
 
-def test_schema_completeness():
+def test_list_schema_completeness():
+    pass
+
+
+def test_get_schema_completeness():
+    pass
+
+
+def test_create_schema_completeness():
+    pass
+
+
+def test_update_schema_completeness():
+    pass
+
+
+def test_partialupdate_schema_completeness():
+    pass
+
+
+def test_destroy_schema_completeness():
+    pass
+
+
+def test_trace_schema_completeness():
     pass
