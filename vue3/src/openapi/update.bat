931865b61f95a7b09149cf2c60c2a0d638dc0bc5
smilerz <smilerz@gmail.com>
2024-04-24 13:44:21 -0500

    restore original openapi update.bat

--- vue3/src/openapi/update.bat
+++ vue3/src/openapi/update.bat
@@ -1,4 +1,1 @@
 openapi-generator-cli generate -g typescript-fetch -i http://127.0.0.1:8000/openapi/
-
-cd ../../../vue/src/utils/openapi/
-openapi-generator-cli generate -g typescript-axios -i http://127.0.0.1:8000/openapi/
