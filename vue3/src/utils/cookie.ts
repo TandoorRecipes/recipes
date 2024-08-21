0eabf1f7c4834c72fd38c0a0a5628654f4e4c5d9
vabene1111 <vabene1234@googlemail.com>
2024-08-21 16:26:13 +0200

    central cookie function

---
+++ vue3/src/utils/cookie.ts
@@ -0,0 +1,19 @@
+/**
+ * simple function to retrieve a cookie by name
+ * @param name name of the cookie
+ */
+export function getCookie(name: string) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
