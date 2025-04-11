675d7a0647ece3230c0c12a80945b8c2611f565c
vabene1111 <vabene1234@googlemail.com>
2025-04-11 14:12:37 +0200

    model select show icon only on mobile for create indicator

--- vue3/src/components/inputs/ModelSelect.vue
+++ vue3/src/components/inputs/ModelSelect.vue
@@ -40,8 +40,9 @@
           <template #option="{ option }" v-if="props.allowCreate">
             <div class="d-flex align-center justify-space-between w-100">
               <span>{{ option[itemLabel] }}</span>
-              <v-chip size="x-small" :color="option.__CREATE__ ? 'success' : 'primary'" class="ml-2">
-                {{ option.__CREATE__ ? $t("New") : $t("Existing") }}
+              <v-chip size="x-small" color="create" class="ml-2" v-if="option.__CREATE__">
+                  <v-icon icon="$create"></v-icon>
+                  <template class="d-none d-lg-block">{{$t('Create')}}</template>
               </v-chip>
             </div>
           </template>
