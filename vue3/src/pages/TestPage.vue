c1e56920ec6455b32b96d883fcb263fcebdb1655
vabene1111 <vabene1234@googlemail.com>
2025-04-18 13:31:15 +0200

    prepared test view

--- vue3/src/pages/TestPage.vue
+++ vue3/src/pages/TestPage.vue
@@ -1,17 +1,12 @@
 <template>
     <v-container>
-        <v-row>
-            <v-col>
-                <p>Test P1</p>
-                <p>Test 2P</p>
-                <ol>
-                    <li>Test 1</li>
-                    <li>Test 2</li>
-                    <li>Test 3</li>
-                </ol>
-                <p>Text Bla Bla3</p>
-            </v-col>
-        </v-row>
+       Form
+        <v-form>
+            <ModelSelect model="Food"></ModelSelect>
+        </v-form>
+
+        Non Form
+        <ModelSelect model="Food"></ModelSelect>
 
 
     </v-container>
@@ -23,6 +18,7 @@ import {computed, ref, toRaw, watch,} from "vue";
 import {VDateInput} from 'vuetify/labs/VDateInput'
 import {useRouteQuery} from "@vueuse/router";
 import {DateTime} from "luxon";
+import ModelSelect from "@/components/inputs/ModelSelect.vue";
 
 
 </script>
