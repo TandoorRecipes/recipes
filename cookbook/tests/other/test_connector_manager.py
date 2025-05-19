b2da40421b089d3013f1f64115dbb6afba584e1a
Mikhail Epifanov <mikhail.epifanov@gmail.com>
2025-05-19 22:35:17 +0200

    fix test

--- cookbook/tests/other/test_connector_manager.py
+++ cookbook/tests/other/test_connector_manager.py
@@ -2,7 +2,7 @@ import pytest
 from django.contrib import auth
 from mock.mock import Mock
 
-from cookbook.connectors.connector import Connector
+from cookbook.connectors.connector import Connector, ShoppingListEntryDTO
 from cookbook.connectors.connector_manager import ActionType, run_connectors
 from cookbook.models import Food, ShoppingListEntry
 
@@ -13,13 +13,13 @@ def obj_1(space_1, u1_s1):
     return e
 
 
-@pytest.mark.timeout(10)  # TODO this mark doesn't exist
 @pytest.mark.asyncio
 async def test_run_connectors(space_1, u1_s1, obj_1) -> None:
+    expected_dto = ShoppingListEntryDTO.try_create_from_entry(obj_1)
     connector_mock = Mock(spec=Connector)
 
     await run_connectors([connector_mock], obj_1, ActionType.DELETED)
 
     assert not connector_mock.on_shopping_list_entry_updated.called
     assert not connector_mock.on_shopping_list_entry_created.called
-    connector_mock.on_shopping_list_entry_deleted.assert_called_once_with(space_1, obj_1)
+    connector_mock.on_shopping_list_entry_deleted.assert_called_once_with(expected_dto)
