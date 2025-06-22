edcc5f6441a8f1be6f84d4716e9e04867bc9fe82
vabene1111 <vabene1234@googlemail.com>
2025-06-22 11:37:35 +0200

    big cleanup

--- cookbook/forms.py
+++ cookbook/forms.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 
-
 from allauth.account.forms import ResetPasswordForm, SignupForm
 from allauth.socialaccount.forms import SignupForm as SocialSignupForm
 from django import forms
@@ -14,7 +13,6 @@ from hcaptcha.fields import hCaptchaField
 
 from .models import Comment, InviteLink, Keyword, Recipe, SearchPreference, Space, Storage, Sync, User, UserPreference, ConnectorConfig
 
-
 class SelectWidget(widgets.Select):
     class Media:
         js = ('custom/js/form_select.js',)
@@ -23,49 +21,6 @@ class SelectWidget(widgets.Select):
 class MultiSelectWidget(widgets.SelectMultiple):
     class Media:
         js = ('custom/js/form_multiselect.js',)
-
-
-# Yes there are some stupid browsers that still dont support this but
-# I dont support people using these browsers.
-class DateWidget(forms.DateInput):
-    input_type = 'date'
-
-    def __init__(self, **kwargs):
-        kwargs["format"] = "%Y-%m-%d"
-        super().__init__(**kwargs)
-
-
-class UserNameForm(forms.ModelForm):
-    prefix = 'name'
-
-    class Meta:
-        model = User
-        fields = ('first_name', 'last_name')
-
-        help_texts = {'first_name': _('Both fields are optional. If none are given the username will be displayed instead')}
-
-
-class ExternalRecipeForm(forms.ModelForm):
-    file_path = forms.CharField(disabled=True, required=False)
-    file_uid = forms.CharField(disabled=True, required=False)
-
-    def __init__(self, *args, **kwargs):
-        space = kwargs.pop('space')
-        super().__init__(*args, **kwargs)
-        self.fields['keywords'].queryset = Keyword.objects.filter(space=space).all()
-
-    class Meta:
-        model = Recipe
-        fields = ('name', 'description', 'servings', 'working_time', 'waiting_time', 'file_path', 'file_uid', 'keywords')
-
-        labels = {
-            'name': _('Name'), 'keywords': _('Keywords'), 'working_time': _('Preparation time in minutes'), 'waiting_time': _('Waiting time (cooking/baking) in minutes'),
-            'file_path': _('Path'), 'file_uid': _('Storage UID'),
-        }
-        widgets = {'keywords': MultiSelectWidget}
-        field_classes = {'keywords': SafeModelMultipleChoiceField, }
-
-
 class ImportExportBase(forms.Form):
     DEFAULT = 'DEFAULT'
     PAPRIKA = 'PAPRIKA'
@@ -97,7 +52,6 @@ class ImportExportBase(forms.Form):
                                       (PLANTOEAT, 'Plantoeat'), (COOKBOOKAPP, 'CookBookApp'), (COPYMETHAT, 'CopyMeThat'), (PDF, 'PDF'), (MELARECIPES, 'Melarecipes'),
                                       (COOKMATE, 'Cookmate'), (REZEPTSUITEDE, 'Recipesuite.de'), (GOURMET, 'Gourmet')))
 
-
 class MultipleFileInput(forms.ClearableFileInput):
     allow_multiple_selected = True
 
@@ -121,8 +75,6 @@ class ImportForm(ImportExportBase):
     files = MultipleFileField(required=True)
     duplicates = forms.BooleanField(help_text=_('To prevent duplicates recipes with the same name as existing ones are ignored. Check this box to import everything.'),
                                     required=False)
-
-
 class ExportForm(ImportExportBase):
     recipes = forms.ModelMultipleChoiceField(widget=MultiSelectWidget, queryset=Recipe.objects.none(), required=False)
     all = forms.BooleanField(required=False)
@@ -133,125 +85,7 @@ class ExportForm(ImportExportBase):
         super().__init__(*args, **kwargs)
         self.fields['recipes'].queryset = Recipe.objects.filter(space=space).all()
 
-
-class CommentForm(forms.ModelForm):
-    prefix = 'comment'
-
-    class Meta:
-        model = Comment
-        fields = ('text',)
-
-        labels = {'text': _('Add your comment: '), }
-        widgets = {'text': forms.Textarea(attrs={'rows': 2, 'cols': 15}), }
-
-
-class StorageForm(forms.ModelForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'autocomplete': 'new-password'}), required=False)
-    password = forms.CharField(widget=forms.TextInput(attrs={'autocomplete': 'new-password', 'type': 'password'}),
-                               required=False,
-                               help_text=_('Leave empty for dropbox and enter app password for nextcloud.'))
-    token = forms.CharField(widget=forms.TextInput(attrs={'autocomplete': 'new-password', 'type': 'password'}),
-                            required=False,
-                            help_text=_('Leave empty for nextcloud and enter api token for dropbox.'))
-
-    class Meta:
-        model = Storage
-        fields = ('name', 'method', 'username', 'password', 'token', 'url', 'path')
-
-        help_texts = {'url': _('Leave empty for dropbox and enter only base url for nextcloud (<code>/remote.php/webdav/</code> is added automatically)'), }
-
-
-class ConnectorConfigForm(forms.ModelForm):
-    enabled = forms.BooleanField(
-        help_text="Is the connector enabled",
-        required=False,
-    )
-
-    on_shopping_list_entry_created_enabled = forms.BooleanField(
-        help_text="Enable action for ShoppingListEntry created events",
-        required=False,
-    )
-
-    on_shopping_list_entry_updated_enabled = forms.BooleanField(
-        help_text="Enable action for ShoppingListEntry updated events",
-        required=False,
-    )
-
-    on_shopping_list_entry_deleted_enabled = forms.BooleanField(
-        help_text="Enable action for ShoppingListEntry deleted events",
-        required=False,
-    )
-
-    supports_description_field = forms.BooleanField(
-        help_text="Does the connector todo entity support the description field",
-        initial=True,
-        required=False,
-    )
-
-    update_token = forms.CharField(
-        widget=forms.TextInput(attrs={'autocomplete': 'update-token', 'type': 'password'}),
-        required=False,
-        help_text=_('<a href="https://www.home-assistant.io/docs/authentication/#your-account-profile">Long Lived Access Token</a> for your HomeAssistant instance')
-    )
-
-    url = forms.URLField(
-        required=False,
-        help_text=_('Something like http://homeassistant.local:8123/api'),
-    )
-
-    class Meta:
-        model = ConnectorConfig
-
-        fields = (
-            'name', 'type', 'enabled', 'on_shopping_list_entry_created_enabled', 'on_shopping_list_entry_updated_enabled',
-            'on_shopping_list_entry_deleted_enabled', 'supports_description_field', 'url', 'todo_entity',
-        )
-
-        help_texts = {
-            'url': _('http://homeassistant.local:8123/api for example'),
-        }
-
-
-class SyncForm(forms.ModelForm):
-
-    def __init__(self, *args, **kwargs):
-        space = kwargs.pop('space')
-        super().__init__(*args, **kwargs)
-        self.fields['storage'].queryset = Storage.objects.filter(space=space).all()
-
-    class Meta:
-        model = Sync
-        fields = ('storage', 'path', 'active')
-
-        field_classes = {'storage': SafeModelChoiceField, }
-
-        labels = {'storage': _('Storage'), 'path': _('Path'), 'active': _('Active')}
-
-
-class BatchEditForm(forms.Form):
-    search = forms.CharField(label=_('Search String'))
-    keywords = forms.ModelMultipleChoiceField(queryset=Keyword.objects.none(), required=False, widget=MultiSelectWidget)
-
-    def __init__(self, *args, **kwargs):
-        space = kwargs.pop('space')
-        super().__init__(*args, **kwargs)
-        self.fields['keywords'].queryset = Keyword.objects.filter(space=space).all().order_by('id')
-
-
-class ImportRecipeForm(forms.ModelForm):
-
-    def __init__(self, *args, **kwargs):
-        space = kwargs.pop('space')
-        super().__init__(*args, **kwargs)
-        self.fields['keywords'].queryset = Keyword.objects.filter(space=space).all()
-
-    class Meta:
-        model = Recipe
-        fields = ('name', 'keywords', 'file_path', 'file_uid')
-
-        labels = {'name': _('Name'), 'keywords': _('Keywords'), 'file_path': _('Path'), 'file_uid': _('File ID'), }
-        widgets = {'keywords': MultiSelectWidget}
-        field_classes = {'keywords': SafeModelChoiceField, }
+from .models import InviteLink, SearchPreference, Space, User, UserPreference
 
 
 class InviteLinkForm(forms.ModelForm):
--- cookbook/static/custom/js/form_select.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function () {
-    $('.selectwidget').select2();
-});
