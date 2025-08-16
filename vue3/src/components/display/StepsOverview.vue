4e444b421dfa6b191d08ca26af767fe7231ab690
vabene1111 <vabene1234@googlemail.com>
2025-08-16 13:59:25 +0200

    improved sub recipe rendering

--- vue3/src/components/display/StepsOverview.vue
+++ vue3/src/components/display/StepsOverview.vue
@@ -1,13 +1,24 @@
 <template>
     <v-expansion-panels>
         <v-expansion-panel>
-            <v-expansion-panel-title><i class="far fa-list-alt fa-fw me-2"></i> {{$t('StepsOverview')}}</v-expansion-panel-title>
+            <v-expansion-panel-title><i class="far fa-list-alt fa-fw me-2"></i> {{ $t('StepsOverview') }}</v-expansion-panel-title>
             <v-expansion-panel-text>
                 <v-container>
                     <v-row v-for="(s, i) in props.steps">
                         <v-col class="pa-1" cols="12" md="6">
                             <b v-if="s.showAsHeader">{{ i + 1 }}. {{ s.name }} </b>
                             <ingredients-table v-model="s.ingredients" :ingredient-factor="props.ingredientFactor"></ingredients-table>
+
+                            <template v-if="s.stepRecipe">
+                                <v-card class="ma-2 border-md" prepend-icon="$recipes" :title="s.stepRecipeData.name"
+                                        :to="{name: 'RecipeViewPage', params: {id: s.stepRecipeData.id}}" target="_blank">
+                                    <v-row v-for="subRecipeStep in s.stepRecipeData.steps">
+                                        <v-col>
+                                            <ingredients-table v-model="subRecipeStep.ingredients" :ingredient-factor="props.ingredientFactor"></ingredients-table>
+                                        </v-col>
+                                    </v-row>
+                                </v-card>
+                            </template>
                         </v-col>
                     </v-row>
                 </v-container>
@@ -22,6 +33,7 @@
 import {PropType} from 'vue'
 import {Step} from "@/openapi";
 import IngredientsTable from "@/components/display/IngredientsTable.vue";
+import StepView from "@/components/display/StepView.vue";
 
 const props = defineProps({
     steps: {
