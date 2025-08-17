0de50ee4a41004826865a3758cc6f2a7868408c1
vabene1111 <vabene1234@googlemail.com>
2025-08-17 10:17:37 +0200

    fixed vite config for dev server

--- vue3/vite.config.ts
+++ vue3/vite.config.ts
@@ -6,43 +6,46 @@ import vuetify, {transformAssetUrls} from 'vite-plugin-vuetify'
 import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    base: './',
-    plugins: [
-        vue({
-            template: {transformAssetUrls}
-        }),
-        vuetify({
-            autoImport: true,
-        }),
-        VitePWA({
-            //registerType: 'autoUpdate',
-            strategies: 'injectManifest',
-            srcDir: 'src',
-            filename: 'service-worker.ts',
-        })
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-            vue: fileURLToPath(new URL("./node_modules/vue/dist/vue.esm-bundler.js", import.meta.url)),
+export default defineConfig(({command, mode, isSsrBuild, isPreview}) => {
+    console.log('MODE: ', mode)
+    return {
+        base: mode == 'development' ? '/static/vue3/' : './',
+        plugins: [
+            vue({
+                template: {transformAssetUrls}
+            }),
+            vuetify({
+                autoImport: true,
+            }),
+            VitePWA({
+                //registerType: 'autoUpdate',
+                strategies: 'injectManifest',
+                srcDir: 'src',
+                filename: 'service-worker.ts',
+            })
+        ],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+                vue: fileURLToPath(new URL("./node_modules/vue/dist/vue.esm-bundler.js", import.meta.url)),
+            },
+            extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue',],
         },
-        extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue',],
-    },
-    clearScreen: false,
-    build: {
-        outDir: '../cookbook/static/vue3/',
-        // generate manifest.json in outDir
-        manifest: 'manifest.json',
-        rollupOptions: {
-            // overwrite default .html entry
-            input: [
-                'src/apps/tandoor/main.ts',
-            ],
+        clearScreen: false,
+        build: {
+            outDir: '../cookbook/static/vue3/',
+            // generate manifest.json in outDir
+            manifest: 'manifest.json',
+            rollupOptions: {
+                // overwrite default .html entry
+                input: [
+                    'src/apps/tandoor/main.ts',
+                ],
+            },
         },
-    },
-    server: {
-        host: '0.0.0.0', // only needed to expose dev server to network bound IPs
-        origin: 'http://localhost:5173',
+        server: {
+            host: '0.0.0.0', // only needed to expose dev server to network bound IPs
+            origin: 'http://localhost:5173',
+        }
     }
 })
