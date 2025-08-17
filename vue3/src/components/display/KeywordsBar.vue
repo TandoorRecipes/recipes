c5da7d0e9ec01a85035ae01677c2888b8561f294
vabene1111 <vabene1234@googlemail.com>
2025-08-17 10:39:22 +0200

    made keywords klickable again

--- vue3/src/components/display/KeywordsBar.vue
+++ vue3/src/components/display/KeywordsBar.vue
@@ -2,7 +2,9 @@
     <div v-if="props.keywords">
         <slot name="prepend"></slot>
 
-        <v-chip class="me-1 mb-1" :label="props.label" :color="props.color" :size="props.size" :variant="props.variant" v-for="k in keywords"> {{ k.label }}</v-chip>
+        <v-chip class="me-1 mb-1" :label="props.label" :color="props.color" :size="props.size" :variant="props.variant" v-for="k in keywords"
+                :to="{name: 'SearchPage', query: {keywords: k.id}}"> {{ k.label }}
+        </v-chip>
 
         <slot name="append"></slot>
     </div>
@@ -21,11 +23,11 @@ const props = defineProps({
     variant: {type: String as PropType<NonNullable<"tonal" | "flat" | "text" | "elevated" | "outlined" | "plain"> | undefined>, default: 'tonal'},
     label: {type: Boolean, default: true},
     // maximum number of keywords, 0 for all
-    maxKeywords : {type: Number, default: 0},
+    maxKeywords: {type: Number, default: 0},
 })
 
 const keywords = computed(() => {
-    if (props.maxKeywords > 0){
+    if (props.maxKeywords > 0) {
         return props.keywords?.slice(0, props.maxKeywords)
     } else {
         return props.keywords
