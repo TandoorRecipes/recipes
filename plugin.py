d439ee7ffff9dd33f718fac51d29a0cc0d3c7f33
vabene1111 <vabene1234@googlemail.com>
2025-08-21 18:20:00 +0200

    fix windows issue

--- plugin.py
+++ plugin.py
@@ -9,11 +9,11 @@ if os.path.isdir(PLUGINS_DIRECTORY):
     for d in os.listdir(PLUGINS_DIRECTORY):
         if d != '__pycache__':
             try:
-                subprocess.run(['python', 'setup_repo.py'], shell=True, cwd=os.path.join(BASE_DIR, 'recipes', 'plugins', d))
+                subprocess.run(['python', 'setup_repo.py'], shell=(os.name == 'nt'), cwd=os.path.join(BASE_DIR, 'recipes', 'plugins', d))
             except Exception:
                 traceback.print_exc()
                 print(f'ERROR failed to link plugin {d}')
 
-subprocess.run(['npm', 'install', '--global', 'yarn'], shell=True, cwd=os.path.join(BASE_DIR, 'vue3'))
-subprocess.run(['yarn', 'install'], shell=True, cwd=os.path.join(BASE_DIR, 'vue3'))
-subprocess.run(['yarn', 'build'], shell=True, cwd=os.path.join(BASE_DIR, 'vue3'))
+subprocess.run(['npm', 'install', '--global', 'yarn'], shell=(os.name == 'nt'), cwd=os.path.join(BASE_DIR, 'vue3'))
+subprocess.run(['yarn', 'install'], shell=(os.name == 'nt'), cwd=os.path.join(BASE_DIR, 'vue3'))
+subprocess.run(['yarn', 'build'], shell=(os.name == 'nt'), cwd=os.path.join(BASE_DIR, 'vue3'))
