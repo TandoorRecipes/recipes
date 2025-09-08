741e9eb370ccd7a1b063f4a83567881c4e3c91a3
vabene1111 <vabene1234@googlemail.com>
2025-09-08 08:31:01 +0200

    plugin and hosted fixes

--- vue3/vite.config.ts
+++ vue3/vite.config.ts
@@ -1,12 +1,20 @@
-import {fileURLToPath, URL} from 'node:url'
+import {fileURLToPath, pathToFileURL, URL} from 'node:url'
+
+import {readdirSync} from "fs"
+import {resolve, join} from "path"
+import 'esbuild-register/dist/node'
 
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify, {transformAssetUrls} from 'vite-plugin-vuetify'
 import {VitePWA} from "vite-plugin-pwa";
+import {PluginModule} from "./src/types/Plugins";
+import {readFileSync} from "node:fs";
 
 // https://vitejs.dev/config/
-export default defineConfig(({command, mode, isSsrBuild, isPreview}) => {
+export default defineConfig(async ({command, mode, isSsrBuild, isPreview}) => {
+    const buildInputs = await collectBuildInputs()
+
     return {
         base: mode == 'development' ? '/static/vue3/' : './',
         plugins: [
@@ -40,7 +48,7 @@ export default defineConfig(({command, mode, isSsrBuild, isPreview}) => {
                 // overwrite default .html entry
                 input: [
                     'src/apps/tandoor/main.ts',
-                ],
+                ].concat(buildInputs),
             },
         },
         server: {
@@ -49,3 +57,33 @@ export default defineConfig(({command, mode, isSsrBuild, isPreview}) => {
         }
     }
 })
+
+/**
+ * function to load plugin configs and find additional build inputs
+ */
+async function collectBuildInputs() {
+    const pluginsDir = resolve(__dirname, "src/plugins")
+    const buildInputs: string[] = []
+
+    for (const dir of readdirSync(pluginsDir, {withFileTypes: true})) {
+        if (!dir.isDirectory() && !dir.isSymbolicLink()) continue
+
+        const pluginPath = join(pluginsDir, dir.name, "plugin.ts")
+        try {
+            const code = readFileSync(pluginPath, "utf-8")
+            // Regex to capture buildInputs: [ ... ]
+            const match = code.match(/buildInputs\s*:\s*(\[[^\]]*\])/s)
+            if (match) {
+                const arr = [eval][0](match[1]) as string[]
+                buildInputs.push(...arr)
+            }
+        } catch (err) {
+            console.warn(`Failed to parse plugin at ${pluginPath}:`, err)
+        }
+    }
+
+    console.log('Tandoor Plugin build inputs: ', buildInputs)
+    return buildInputs
+}
+
+
