72551a5a79da0aff2be33ad25b9f4c01a6a54c9d
doylelew <doyle.lewis.a@gmail.com>
2025-11-24 12:32:19 -0500

    Request generator added to cooklang tester

--- cookbook/tests/other/test_cooklang_integration.py
+++ cookbook/tests/other/test_cooklang_integration.py
@@ -1,5 +1,25 @@
-import pytest
+from django.contrib import auth
+from django.test import RequestFactory
+from django_scopes import scope
+
+from cookbook.integration.cooklang import Cooklang
 
 
-def test_cooklang_integration():
-    assert 1 == 5
+def request_generator(u1_s1):
+    user = auth.get_user(u1_s1)
+    space = user.userspace_set.first().space
+    request = RequestFactory()
+    request.user = user
+    request.space = space
+
+    return space, request
+
+
+def test_cooklang_integration(u1_s1):
+
+    space, request = request_generator(u1_s1)
+    with scope(space=space):
+        cooklang_integration = Cooklang(request, "export")
+        recipe = cooklang_integration.get_recipe_from_file("")
+
+        assert recipe == 5
