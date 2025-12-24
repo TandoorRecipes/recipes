edd584efef6751d73acd8fc920bd0730cc098596
Steve Wang <stevew77@gmail.com>
2025-12-24 21:03:55 +0800

    fix: update import to match new schema

--- cookbook/integration/cookbookapp.py
+++ cookbook/integration/cookbookapp.py
@@ -2,71 +2,108 @@ import re
 from io import BytesIO
 
 import requests
+import yaml
 
 from cookbook.helper.HelperFunctions import validate_import_url
 from cookbook.helper.ingredient_parser import IngredientParser
-from cookbook.helper.recipe_url_import import (get_from_scraper, get_images_from_soup,
-                                               iso_duration_to_minutes)
-from recipe_scrapers import scrape_html
 from cookbook.integration.integration import Integration
-from cookbook.models import Ingredient, Recipe, Step
+from cookbook.models import NutritionInformation, Keyword, Ingredient, Recipe, Step
+from cookbook.helper.recipe_url_import import parse_servings, parse_servings_text, parse_time
 
 
 class CookBookApp(Integration):
 
     def import_file_name_filter(self, zip_info_object):
-        return zip_info_object.filename.endswith('.html')
+        return zip_info_object.filename.endswith('.yml')
 
     def get_recipe_from_file(self, file):
-        recipe_html = file.getvalue().decode("utf-8")
+        recipe_json = yaml.safe_load(file.getvalue())
 
-        scrape = scrape_html(html=recipe_html, org_url="https://cookbookapp.import", supported_only=False)
-        recipe_json = get_from_scraper(scrape, self.request)
-        images = list(dict.fromkeys(get_images_from_soup(scrape.soup, None)))
+        description = recipe_json['description'].strip()
+        description = recipe_json['description'][:500] if len(description) > 500 else description
 
         recipe = Recipe.objects.create(
             name=recipe_json['name'].strip(),
-            created_by=self.request.user, internal=True,
-            space=self.request.space)
+            description=description,
+            created_by=self.request.user, 
+            internal=True,
+            space=self.request.space,
+            )
 
-        try:
-            recipe.servings = re.findall('([0-9])+', recipe_json['recipeYield'])[0]
-        except Exception:
-            pass
+        # Start by appending all instructions into steps
+        for s in recipe_json['directions']:
+            step = Step.objects.create(instruction=s, space=self.request.space, show_ingredients_table=self.request.user.userpreference.show_step_ingredients, )
+            recipe.steps.add(step)
 
-        try:
-            recipe.working_time = iso_duration_to_minutes(recipe_json['prepTime'])
-            recipe.waiting_time = iso_duration_to_minutes(recipe_json['cookTime'])
-        except Exception:
-            pass
+        # Append everything after to the first step (or create one if empty)
+        step = recipe.steps.first()
+        if not step:  # if there is no step in the exported data
+            step = Step.objects.create(instruction='', space=self.request.space, )
+            recipe.steps.add(step)
 
-        # assuming import files only contain single step
-        step = Step.objects.create(instruction=recipe_json['steps'][0]['instruction'], space=self.request.space,
-                                   show_ingredients_table=self.request.user.userpreference.show_step_ingredients, )
+        # Tandoor doesn't have notes, append it to the end of steps
+        if 'notes' in recipe_json and len(recipe_json['notes']) > 0:
+            notes_text = f"#### Notes\n\n{recipe_json['notes']}"
+            step = Step.objects.create(
+                instruction=notes_text, space=self.request.space,
+            )
+            recipe.steps.add(step)
+            
+        if 'tags' in recipe_json and len(recipe_json['tags']) > 0:
+            for tag in recipe_json['tags']:
+                keyword, _ = Keyword.objects.get_or_create(name=tag.strip(), space=self.request.space)
+                recipe.keywords.add(keyword)
 
+        if 'prep_time' in recipe_json and recipe_json['prep_time'] is not None:
+            recipe.waiting_time = parse_time(recipe_json['prep_time'])
+            
+        if 'cook_time' in recipe_json and recipe_json['cook_time'] is not None:
+            recipe.working_time = parse_time(recipe_json['cook_time'])
+            
+        if 'servings' in recipe_json and recipe_json['servings'] is not None:
+            recipe.servings = parse_servings(recipe_json['servings'])
+            recipe.servings_text = parse_servings_text(recipe_json['servings'])
+
+        if 'source' in recipe_json and recipe_json['source'] is not None:
+            recipe.source_url = recipe_json['source']
+            
+        # Attempt to parse through nutrition list
         if 'nutrition' in recipe_json:
-            step.instruction = step.instruction + '\n\n' + recipe_json['nutrition']
+            nutrition = {}
+            nutrition_list = recipe_json['nutrition'].lower().splitlines()
+            for item in nutrition_list:
+                try:
+                    if 'calories' in item:
+                        nutrition['calories'] = int(re.search(r'\d+', item).group())
+                    if 'protein' in item:
+                        nutrition['proteins'] = int(re.search(r'\d+', item).group())
+                    if 'fat' in item:
+                        nutrition['fats'] = int(re.search(r'\d+', item).group())
+                    if 'carb' in item:
+                        nutrition['carbohydrates'] = int(re.search(r'\d+', item).group())
 
-        step.save()
-        recipe.steps.add(step)
-
+                    if nutrition != {}:
+                        recipe.nutrition = NutritionInformation.objects.create(**nutrition, space=self.request.space)
+                        recipe.save()
+                except Exception:
+                    pass
+    
+        # Attempt to parse through each ingredient (these are free-form so anything goes)
         ingredient_parser = IngredientParser(self.request, True)
-        for ingredient in recipe_json['steps'][0]['ingredients']:
-            f = ingredient_parser.get_food(ingredient['food']['name'])
-            u = None
-            if unit := ingredient.get('unit', None):
-                u = ingredient_parser.get_unit(unit.get('name', None))
+        for ingredient in recipe_json['ingredients']:
+            if not ingredient:
+                continue
+            amount, unit, food, note = ingredient_parser.parse(ingredient)
             step.ingredients.add(Ingredient.objects.create(
-                food=f, unit=u, amount=ingredient.get('amount', None), note=ingredient.get('note', None), original_text=ingredient.get('original_text', None), space=self.request.space,
+                food=food, unit=unit, amount=amount, note=note, original_text=ingredient, space=self.request.space,
             ))
 
+        # Try to import an image link, this may be blocked by cors
         try:
-            for url in images:
-                # import the first valid image which is not cookbookapp branding
-                if validate_import_url(url) and not url.startswith("https://media.cookbookmanager.com/brand/"):
-                    response = requests.get(url)
-                    self.import_recipe_image(recipe, BytesIO(response.content))
-                    break
+            url = recipe_json["image"]
+            if validate_import_url(url):
+                response = requests.get(url)
+                self.import_recipe_image(recipe, BytesIO(response.content))
         except Exception as e:
             print('failed to import image ', str(e))
 
