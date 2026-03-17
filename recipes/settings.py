75b348c1f73e8a78afa35e13e68698716c31a560
vabene1111 <vabene1234@googlemail.com>
2026-03-17 17:15:26 +0100

    remove API basic auth

--- recipes/settings.py
+++ recipes/settings.py
@@ -371,7 +371,8 @@ WRITE_SCOPE = 'write'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication', 'oauth2_provider.contrib.rest_framework.OAuth2Authentication', 'rest_framework.authentication.BasicAuthentication'
+        'rest_framework.authentication.SessionAuthentication',
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
